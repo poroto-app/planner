187d6c653c2bc748149370567050d573e8758ac3
0re2236 <2236takahashi@gmail.com>
2023-03-18 13:11:06 +0900

    contains関数の定義

--- internal/infrastructure/api/google/places.go
+++ internal/infrastructure/api/google/places.go
@@ -24,6 +24,18 @@ func init() {
 	}
 }
 
+// Check whether including or not
+func contains(target, words []string) bool {
+	for _, element := range target {
+		for _, word := range words {
+			if element == word {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func main() {
 	opt := maps.WithAPIKey(os.Getenv("GOOGLE_PLACES_API_KEY"))
 	c, err := maps.NewClient(opt)
