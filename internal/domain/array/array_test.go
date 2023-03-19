c3ea66a067c64da756e5f4240353a50910a19a5d
0re2236 <2236takahashi@gmail.com>
2023-03-19 15:54:09 +0900

    IsContainのテスト作成

--- internal/domain/array/array_test.go
+++ internal/domain/array/array_test.go
@@ -36,3 +36,37 @@ func TestHasIntersection(t *testing.T) {
 		})
 	}
 }
+func TestIsContain(t *testing.T) {
+	cases := []struct {
+		name     string
+		array    []string
+		value    string
+		expected bool
+	}{
+		{
+			name:     "is contain",
+			array:    []string{"a", "b", "c"},
+			value:    "a",
+			expected: true,
+		}, {
+			name:     "is not contain",
+			array:    []string{"a", "b", "c"},
+			value:    "d",
+			expected: false,
+		}, {
+			name:     "empty array",
+			array:    []string{},
+			value:    "a",
+			expected: false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			result := IsContain(c.array, c.value)
+			if result != c.expected {
+				t.Errorf("expected: %v, result: %v", c.expected, result)
+			}
+		})
+	}
+}
