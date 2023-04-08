bdf665310b236aead3117e9e9ac00ef77368c304
0re2236 <2236takahashi@gmail.com>
2023-04-08 18:57:06 +0900

    url builder関数を作成

--- internal/infrastructure/api/google/places/photos.go
+++ internal/infrastructure/api/google/places/photos.go
@@ -3,21 +3,45 @@ package places
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"path"
 )
 
 type PlacePhoto struct {
 	ImageUrl string
 }
 
+func ImgUrlBuilder(maxWidth int, maxHeight int, photoReference string, apiKey string) (string, error) {
+	u, err := url.Parse("https://maps.goog;eapis.com")
+	if err != nil {
+		return "", err
+	}
+
+	u.Path = path.Join(u.Path, "maps", "api", "place", "photo")
+	q := u.Query()
+
+	q.Set("maxwidth", fmt.Sprint(maxWidth))
+	q.Set("maxHeight", fmt.Sprint(maxHeight))
+	q.Set("photo_reference", photoReference)
+	q.Set("key", apiKey)
+
+	u.RawQuery = q.Encode()
+	return u.String(), nil
+}
+
 func (r PlacesApi) FetchPlacePhotos(ctx context.Context, place Place) ([]PlacePhoto, error) {
 	const maxWidth int = 400
 	const maxHeight int = 400
-	const placePhotoApi string = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=%d&maxheight=%d&photo_reference=%s&key=%s"
 	var placePhotos []PlacePhoto
 
 	for _, photoReference := range place.photoReferences {
+		url, err := ImgUrlBuilder(maxWidth, maxHeight, photoReference, r.apiKey)
+		if err != nil {
+			return nil, err
+		}
+
 		placePhotos = append(placePhotos, PlacePhoto{
-			ImageUrl: fmt.Sprintf(placePhotoApi, maxWidth, maxHeight, photoReference, r.apiKey),
+			ImageUrl: url,
 		})
 	}
 
