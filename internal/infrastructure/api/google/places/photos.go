aba2f8dcac7b9adacfeeaa6ad30a5e674c70a32d
0re2236 <2236takahashi@gmail.com>
2023-04-08 17:19:30 +0900

    FetchPlacePhotos()の実装

--- internal/infrastructure/api/google/places/photos.go
+++ internal/infrastructure/api/google/places/photos.go
@@ -1,6 +1,9 @@
 package places
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type PlacePhoto struct {
 	ImageUrl string
@@ -8,5 +11,16 @@ type PlacePhoto struct {
 
 func (r PlacesApi) FetchPlacePhotos(ctx context.Context, place Place) ([]PlacePhoto, error) {
 	// TODO: implement me
-	return []PlacePhoto{}, nil
+	const maxWidth int = 400
+	const maxHeight int = 400
+	const placePhotoApi string = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=%d&maxheight=%d&photo_reference=%s&key=%s"
+	var placePhotos []PlacePhoto
+
+	for _, photoReference := range place.photoReferences {
+		placePhotos = append(placePhotos, PlacePhoto{
+			ImageUrl: fmt.Sprintf(placePhotoApi, maxWidth, maxHeight, photoReference, r.apiKey),
+		})
+	}
+
+	return placePhotos, nil
 }
