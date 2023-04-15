4da361d7a862b7ad6f3302f20814687d398db3fd
ishizakamasaki <robomasaki2@gmail.com>
2023-04-15 09:54:59 +0900

    feat: 距離によるフィルタのテストを追加

--- internal/domain/services/plan_test.go
+++ internal/domain/services/plan_test.go
@@ -94,3 +94,60 @@ func TestFilterByOpeningNow(t *testing.T) {
 		})
 	}
 }
+
+func TestFilterWithinDistanceRange(t *testing.T) {
+	cases := []struct {
+		name            string
+		placesToFilter  []places.Place
+		currentLocation models.GeoLocation
+		startInMeter    float64
+		endInMeter      float64
+		expected        []places.Place
+	}{
+		{
+			name: "should filter places by distance range",
+			placesToFilter: []places.Place{
+				{
+					Name: "Tokyo Sky Tree",
+					Location: places.Location{
+						Latitude:  35.710063,
+						Longitude: 139.8107,
+					},
+				},
+				{
+					Name: "Tokyo Tower",
+					Location: places.Location{
+						Latitude:  35.658581,
+						Longitude: 139.745433,
+					},
+				},
+			},
+			// とうきょうスカイツリー駅
+			currentLocation: models.GeoLocation{
+				Latitude:  35.7104,
+				Longitude: 139.8093,
+			},
+			startInMeter: 0,
+			endInMeter:   500,
+			expected: []places.Place{
+				{
+					Name: "Tokyo Sky Tree",
+					Location: places.Location{
+						Latitude:  35.710063,
+						Longitude: 139.8107,
+					},
+				},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			s := PlanService{}
+			actual := s.filterWithinDistanceRange(c.placesToFilter, c.currentLocation, c.startInMeter, c.endInMeter)
+			if !reflect.DeepEqual(actual, c.expected) {
+				t.Errorf("expected %v, got %v", c.expected, actual)
+			}
+		})
+	}
+}
