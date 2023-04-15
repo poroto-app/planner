17dd1348e72c5098690a3a602e1b979f3535a98d
zacker <robomasaki2@gmail.com>
2023-04-15 20:45:47 +0900

    feat: カテゴリ一覧を返す

--- graphql/resolver/plan.resolvers.go
+++ graphql/resolver/plan.resolvers.go
@@ -6,7 +6,6 @@ package resolver
 
 import (
 	"context"
-	"fmt"
 
 	"poroto.app/poroto/planner/graphql/model"
 )
@@ -19,5 +18,19 @@ func (r *mutationResolver) CreatePlanByLocation(ctx context.Context, input *mode
 
 // MatchInterests is the resolver for the matchInterests field.
 func (r *queryResolver) MatchInterests(ctx context.Context, input *model.MatchInterestsInput) (*model.InterestCandidate, error) {
-	panic(fmt.Errorf("not implemented: MatchInterests - matchInterests"))
+	// TODO: 実際に付近の場所のカテゴリを提示する
+	return &model.InterestCandidate{
+		Categories: []*model.LocationCategory{
+			{
+				Name:        "spa",
+				DisplayName: "温泉",
+				Photo:       "https://images.pexels.com/photos/347137/pexels-photo-347137.jpeg",
+			},
+			{
+				Name:        "cafe",
+				DisplayName: "カフェ",
+				Photo:       "https://images.pexels.com/photos/1402407/pexels-photo-1402407.jpeg",
+			},
+		},
+	}, nil
 }
