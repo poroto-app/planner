295d5dd7af8261f832c694ff31c7879839ccb0e4
zacker <robomasaki2@gmail.com>
2023-04-15 14:49:20 +0900

    feature: カテゴリの数を増やした

--- internal/domain/models/category.go
+++ internal/domain/models/category.go
@@ -6,13 +6,23 @@ type LocationCategory struct {
 }
 
 var (
+	// SEE: https://developers.google.com/maps/documentation/places/web-service/supported_types?hl=ja#table1
 	CategoryAmusements = LocationCategory{
 		Name: "amusements",
 		SubCategories: []string{
 			"amusement_park",
 			"aquarium",
 			"art_gallery",
+			"bowling_alley",
+			"campground",
+			"movie_rental",
+			"movie_theater",
 			"museum",
+			"park",
+			"rv_park",
+			"spa",
+			"stadium",
+			"tourist_attraction",
 		},
 	}
 
@@ -26,4 +36,27 @@ var (
 			"restaurant",
 		},
 	}
+
+	CategoryBook = LocationCategory{
+		Name: "Book",
+		SubCategories: []string{
+			"book_store",
+			"library",
+		},
+	}
+
+	CategoryShopping = LocationCategory{
+		Name: "Shopping",
+		SubCategories: []string{
+			"clothing_store",
+			"department_store",
+			"furniture_store",
+			"hardware_store",
+			"home_goods_store",
+			"shoe_store",
+			"store",
+		},
+	}
+
+	CategoryOutdoor = LocationCategory{}
 )
