740fd5164b2d7103e8bf52e2e0f14168fdf015b5
0re2236 <2236takahashi@gmail.com>
2023-04-16 11:29:58 +0900

    ””はペアでないといけない（戒め）

--- internal/domain/models/plan.go
+++ internal/domain/models/plan.go
@@ -3,5 +3,5 @@ package models
 type Plan struct {
 	Name          string  `json:"name"`
 	Places        []Place `json:"places"`
-	TimeInMinutes float64 `json:"time_in_minutes` // MEMO: 複数プレイスを扱うようになったら，区間ごとの移動時間も保持したい
+	TimeInMinutes float64 `json:"time_in_minutes"` // MEMO: 複数プレイスを扱うようになったら，区間ごとの移動時間も保持したい
 }
