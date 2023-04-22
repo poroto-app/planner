6e5adcbe8b42d786e51e381cd499a5f1381c0c89
ishizakamasaki <robomasaki2@gmail.com>
2023-04-22 12:27:56 +0900

    feat: サムネイル画像を設定できるようにする

--- internal/domain/models/place.go
+++ internal/domain/models/place.go
@@ -1,7 +1,10 @@
 package models
 
+// Place 場所の情報
+// Thumbnail サムネイル用の低画質な写真
 type Place struct {
-	Name     string      `json:"name"`
-	Location GeoLocation `json:"location"`
-	Photos   []string    `json:"photos"`
+	Name      string      `json:"name"`
+	Location  GeoLocation `json:"location"`
+	Thumbnail *string     `json:"thumbnail"`
+	Photos    []string    `json:"photos"`
 }
