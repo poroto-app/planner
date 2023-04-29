f266ee4823b28a5f8316ff5dda908ba4d12cec04
0re2236 <2236takahashi@gmail.com>
2023-04-29 15:59:19 +0900

    CategoryOfSubCategory関数のテスト

--- internal/domain/models/category_test.go
+++ internal/domain/models/category_test.go
@@ -32,3 +32,32 @@ func TestGetCategoryOfName(t *testing.T) {
 		})
 	}
 }
+
+func TestCategoryOfSubCategory(t *testing.T) {
+	cases := []struct {
+		name        string
+		subCategory string
+		expected    LocationCategory
+	}{
+
+		{
+			name:        "cafe belongs to CategoryCafe",
+			subCategory: "cafe",
+			expected:    CategoryCafe,
+		},
+		{
+			name:        "shoe_store belongs to CategoryShopping",
+			subCategory: "shoe_store",
+			expected:    CategoryShopping,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			resultCategory := CategoryOfSubCategory(c.subCategory)
+			if !reflect.DeepEqual(*resultCategory, c.expected) {
+				t.Errorf("expected: %v\nactual: %v", *resultCategory, c.expected)
+			}
+		})
+	}
+}
