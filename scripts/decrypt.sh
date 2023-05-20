ac748027373aac117361581fd1ae471abc7c0791
zacker <robomasaki2@gmail.com>
2023-05-20 15:56:46 +0900

    feat(ci): 環境ごとのシークレットを複合するスクリプトを作成

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,11 +1,53 @@
-# ローカルで実行するときはこの行のコメントアウトを外す
-#git clone --depth 1 git@github.com:poroto-app/infrastructure.git
+function decrypt() {
+  environment=$1
+  file=$2
 
-echo Decrypt Secrets && \
+  echo "[${environment}] Decrypting ${file} ..."
   gcloud kms decrypt \
     --location "asia-northeast1" \
     --keyring "planner_api_key_ring" \
     --key "planner_api_crypt_key" \
-    --plaintext-file ./.env.local \
-    --ciphertext-file ./infrastructure/roles/app/planner/production/.env.local.enc && \
-  rm -rf infrastructure
+    --plaintext-file "${file}" \
+    --ciphertext-file "./infrastructure/roles/app/planner/${environment}/${file}.enc" || exit 1
+  echo ">> [${environment}] Decrypted ${file}!"
+}
+
+# infrastructureリポジトリをclone
+if [ ! -d infrastructure ]; then
+  git clone --depth 1 git@github.com:poroto-app/infrastructure.git
+fi
+
+# 環境設定（デフォルトはdevelopment）
+env=$1
+if [ -z "${env}" ]; then
+  env="development"
+fi
+
+# 共通のシークレットを復号化
+secrets=(".env.local")
+for file in "${secrets[@]}"; do
+  decrypt "production" "${file}"
+done
+
+# 環境ごとのシークレットを復号化
+if [ ! -d secrets ]; then
+  mkdir secrets
+fi
+
+case "$env" in
+"development")
+  secrets_variable=("secrets/google-credential.json.dev")
+  ;;
+"staging")
+  secrets_variable=("secrets/google-credential.json.stg")
+  ;;
+"production")
+  secrets_variable=("secrets/google-credential.json.prd")
+  ;;
+esac
+
+for file in "${secrets_variable[@]}"; do
+  decrypt "${env}" "${file}"
+done
+
+rm -rf infrastructure
