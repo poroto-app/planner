0017a38d2b65aec84fa1ff17ca6528fc8848e1fa
zacker <robomasaki2@gmail.com>
2023-06-03 18:59:33 +0900

    feat(domain): フィルタリング関数を作成

--- internal/domain/models/places_filter.go
+++ internal/domain/models/places_filter.go
@@ -1,6 +1,9 @@
 package models
 
-import "poroto.app/poroto/planner/internal/infrastructure/api/google/places"
+import (
+	"poroto.app/poroto/planner/internal/domain/array"
+	"poroto.app/poroto/planner/internal/infrastructure/api/google/places"
+)
 
 type PlacesFilter struct {
 	placesToFilter []places.Place
@@ -16,6 +19,16 @@ func (f PlacesFilter) Places() []places.Place {
 	return f.placesToFilter
 }
 
+func (f PlacesFilter) Find(findFunc func(place places.Place) bool) *places.Place {
+	for _, place := range f.placesToFilter {
+		if findFunc(place) {
+			copy := place
+			return &copy
+		}
+	}
+	return nil
+}
+
 func (f PlacesFilter) filterPlaces(filterFunc func(place places.Place) bool) []places.Place {
 	var values []places.Place
 	for _, place := range f.placesToFilter {
@@ -24,4 +37,41 @@ func (f PlacesFilter) filterPlaces(filterFunc func(place places.Place) bool) []p
 		}
 	}
 	return values
+}
+
+func (f PlacesFilter) FilterByCategory(categories []LocationCategory) PlacesFilter {
+	var subCategories []string
+	for _, category := range categories {
+		subCategories = append(subCategories, category.SubCategories...)
+	}
+
+	f.placesToFilter = f.filterPlaces(func(place places.Place) bool {
+		for _, category := range place.Types {
+			if array.IsContain(subCategories, category) {
+				return true
+			}
+		}
+		return false
+	})
+
+	return f
+}
+
+func (f PlacesFilter) FilterByOpeningNow() PlacesFilter {
+	f.placesToFilter = f.filterPlaces(func(place places.Place) bool {
+		return place.OpenNow
+	})
+	return f
+}
+
+func (f PlacesFilter) FilterWithinDistanceRange(
+	currentLocation GeoLocation,
+	startInMeter float64,
+	endInMeter float64,
+) PlacesFilter {
+	f.placesToFilter = f.filterPlaces(func(place places.Place) bool {
+		distance := currentLocation.DistanceInMeter(place.Location.ToGeoLocation())
+		return startInMeter <= distance && distance < endInMeter
+	})
+	return f
 }
