157b0fad413eebe5b1c0286ed8ac80688823d247
zacker <robomasaki2@gmail.com>
2023-06-03 13:56:36 +0900

    feat(domain): プランを現在地から作成するかを指定する

--- graphql/resolver/plan.resolvers.go
+++ graphql/resolver/plan.resolvers.go
@@ -37,7 +37,7 @@ func (r *mutationResolver) CreatePlanByLocation(ctx context.Context, input model
 
 	session := uuid.New().String()
 
-	if err := service.CachePlanCandidate(ctx, session, *plans); err != nil {
+	if err := service.CachePlanCandidate(ctx, session, *plans, true); err != nil {
 		log.Println("error while caching plan candidate: ", err)
 	}
 
