f70956c3e520dae116dc3d8a1efa796b1ad5d4e7
zacker <robomasaki2@gmail.com>
2023-06-03 18:51:33 +0900

    feat(domain): Placeオブジェクトをフィルタリングするための構造体を作成

---
+++ internal/domain/models/places_filter.go
@@ -0,0 +1,27 @@
+package models
+
+import "poroto.app/poroto/planner/internal/infrastructure/api/google/places"
+
+type PlacesFilter struct {
+	placesToFilter []places.Place
+}
+
+func NewPlacesFilter(placesToFilter []places.Place) PlacesFilter {
+	return PlacesFilter{
+		placesToFilter: placesToFilter,
+	}
+}
+
+func (f PlacesFilter) Places() []places.Place {
+	return f.placesToFilter
+}
+
+func (f PlacesFilter) filterPlaces(filterFunc func(place places.Place) bool) []places.Place {
+	var values []places.Place
+	for _, place := range f.placesToFilter {
+		if filterFunc(place) {
+			values = append(values, place)
+		}
+	}
+	return values
+}
