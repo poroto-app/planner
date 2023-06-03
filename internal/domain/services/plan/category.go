281c4dfb343aa2044a1f848bfd15aa92fec3f94f
zacker <robomasaki2@gmail.com>
2023-06-03 18:39:43 +0900

    refactor(domain): ループ内で利用する変数をループの直上に配置する

--- internal/domain/services/plan/category.go
+++ internal/domain/services/plan/category.go
@@ -31,16 +31,15 @@ func (s PlanService) CategoriesNearLocation(
 	// TODO: 現在時刻でフィルタリングするかを指定できるようにする
 	placesSearched = s.filterByOpeningNow(placesSearched)
 
-	// 検索された場所のカテゴリとその写真を取得
-	categories := make([]models.LocationCategory, 0)
-	placesUsedOfCategory := make([]places.Place, 0)
-
-	// 場所をカテゴリごとにグループ化し、場所の少ないカテゴリから順に写真を取得する
+	// 場所をカテゴリごとにグループ化し、対応する場所の少ないカテゴリから順に写真を取得する
 	placeCategoryGroups := groupPlacesByCategory(placesSearched)
 	sort.Slice(placeCategoryGroups, func(i, j int) bool {
 		return len(placeCategoryGroups[i].places) < len(placeCategoryGroups[j].places)
 	})
 
+	// 検索された場所のカテゴリとその写真を取得
+	categories := make([]models.LocationCategory, 0)
+	placesUsedOfCategory := make([]places.Place, 0)
 	for _, categoryPlaces := range placeCategoryGroups {
 		category := models.GetCategoryOfName(categoryPlaces.category)
 		if category == nil {
