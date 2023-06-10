c7d96225ff9c696d01ad6247e3c6ccf088226ed3
zacker <robomasaki2@gmail.com>
2023-06-10 13:38:23 +0900

    feat(domain): 冪等性を保つための処理を追加

--- internal/domain/services/plan/save.go
+++ internal/domain/services/plan/save.go
@@ -3,11 +3,13 @@ package plan
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"poroto.app/poroto/planner/internal/domain/models"
 )
 
 func (s PlanService) SavePlanFromPlanCandidate(ctx context.Context, planCandidateId string, planId string) (*models.Plan, error) {
+	// プラン候補から対応するプランを取得
 	planCandidate, err := s.planCandidateRepository.Find(ctx, planCandidateId)
 	if err != nil {
 		return nil, err
@@ -24,6 +26,18 @@ func (s PlanService) SavePlanFromPlanCandidate(ctx context.Context, planCandidat
 		return nil, fmt.Errorf("plan(%v) not found in plan candidate(%v)", planId, planCandidateId)
 	}
 
+	// 冪等性を保つために、既存のプランを取得してから保存する
+	planSaved, err := s.planRepository.Find(planId)
+	if err != nil {
+		// ログに出力するが、エラーは返さない
+		log.Println(fmt.Errorf("error while finding plan(%v): %v", planId, err))
+	}
+
+	if planSaved != nil {
+		return planSaved, nil
+	}
+
+	// プランを保存
 	if err := s.planRepository.Save(planToSave); err != nil {
 		return nil, err
 	}
