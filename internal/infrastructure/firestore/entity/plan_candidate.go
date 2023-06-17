e766145fc0ae8ba8c21f1f819251f1d3f97d2c95
zacker <robomasaki2@gmail.com>
2023-06-17 15:53:38 +0900

    refactor: PlanInCandidateにリネーム

--- internal/infrastructure/firestore/entity/plan_candidate.go
+++ internal/infrastructure/firestore/entity/plan_candidate.go
@@ -7,16 +7,16 @@ import (
 )
 
 type PlanCandidateEntity struct {
-	Id                            string                        `firestore:"id"`
-	Plans                         []PlanInternalCandidateEntity `firestore:"plans"`
-	CreatedBasedOnCurrentLocation bool                          `firestore:"created_based_on_current_location"`
-	ExpiresAt                     time.Time                     `firestore:"expires_at"`
+	Id                            string                  `firestore:"id"`
+	Plans                         []PlanInCandidateEntity `firestore:"plans"`
+	CreatedBasedOnCurrentLocation bool                    `firestore:"created_based_on_current_location"`
+	ExpiresAt                     time.Time               `firestore:"expires_at"`
 }
 
 func ToPlanCandidateEntity(planCandidate models.PlanCandidate) PlanCandidateEntity {
-	plans := make([]PlanInternalCandidateEntity, len(planCandidate.Plans))
+	plans := make([]PlanInCandidateEntity, len(planCandidate.Plans))
 	for i, plan := range planCandidate.Plans {
-		plans[i] = toPlanCandidateEntity(
+		plans[i] = toPlanInCandidateEntity(
 			plan.Id,
 			plan.Name,
 			plan.Places,
@@ -35,7 +35,7 @@ func ToPlanCandidateEntity(planCandidate models.PlanCandidate) PlanCandidateEnti
 func FromPlanCandidateEntity(entity PlanCandidateEntity) models.PlanCandidate {
 	plans := make([]models.Plan, len(entity.Plans))
 	for i, plan := range entity.Plans {
-		plans[i] = fromPlanInternalCandidateEntity(
+		plans[i] = fromPlanInCandidateEntity(
 			plan.Id,
 			plan.Name,
 			plan.Places,
--- internal/infrastructure/firestore/entity/plan_in_candidate.go
+++ internal/infrastructure/firestore/entity/plan_in_candidate.go
@@ -4,9 +4,9 @@ import (
 	"poroto.app/poroto/planner/internal/domain/models"
 )
 
-// PlanInternalCandidateEntity PlanCandidateEntityに含まれるPlan
+// PlanInCandidateEntity PlanCandidateEntityに含まれるPlan
 // MEMO: PlanEntityを用いると、CreatedAtとUpdatedAtが含まれてしまうため、別の構造体を利用している
-type PlanInternalCandidateEntity struct {
+type PlanInCandidateEntity struct {
 	Id     string        `firestore:"id"`
 	Name   string        `firestore:"name"`
 	Places []PlaceEntity `firestore:"places"`
@@ -14,18 +14,18 @@ type PlanInternalCandidateEntity struct {
 	TimeInMinutes int `firestore:"time_in_minutes"`
 }
 
-func toPlanCandidateEntity(
+func toPlanInCandidateEntity(
 	id string,
 	name string,
 	places []models.Place,
 	timeInMinutes uint,
-) PlanInternalCandidateEntity {
+) PlanInCandidateEntity {
 	ps := make([]PlaceEntity, len(places))
 	for i, place := range places {
 		ps[i] = ToPlaceEntity(place)
 	}
 
-	return PlanInternalCandidateEntity{
+	return PlanInCandidateEntity{
 		Id:            id,
 		Name:          name,
 		Places:        ps,
@@ -33,7 +33,7 @@ func toPlanCandidateEntity(
 	}
 }
 
-func fromPlanInternalCandidateEntity(
+func fromPlanInCandidateEntity(
 	id string,
 	name string,
 	places []PlaceEntity,
