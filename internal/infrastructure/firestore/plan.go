0bf10eeb0d46aa8a5af4fdf7cbc15e1a6608664a
zacker <robomasaki2@gmail.com>
2023-06-17 18:23:49 +0900

    feat(data): ソートされたプラン一覧を取得するメソッドを作成

--- internal/infrastructure/firestore/plan.go
+++ internal/infrastructure/firestore/plan.go
@@ -71,6 +71,44 @@ func (p *PlanRepository) Find(ctx context.Context, planId string) (*models.Plan,
 	return &plan, nil
 }
 
+// SortedByCreatedAt created_atで降順に並べたPlanを取得する
+// queryCursor(リストの最後の [models.Plan] のID)が指定されている場合は、そのcursor以降のPlanを取得する
+func (p *PlanRepository) SortedByCreatedAt(ctx context.Context, queryCursor *string, limit int) ([]models.Plan, error) {
+	collection := p.collection()
+	query := collection.OrderBy("created_at", firestore.Desc).OrderBy("id", firestore.Desc)
+	if queryCursor != nil {
+		plan, err := p.find(ctx, *queryCursor)
+		if err != nil {
+			return nil, fmt.Errorf("error while getting plans: %v", err)
+		}
+
+		if plan == nil {
+			return nil, fmt.Errorf("plan of queryCursor(%s) was not found", *queryCursor)
+		}
+
+		query = query.StartAfter(plan.CreatedAt, plan.Id)
+	}
+	query = query.Limit(limit)
+
+	snapshots, err := query.Documents(ctx).GetAll()
+	if err != nil {
+		return nil, fmt.Errorf("error while getting plans: %v", err)
+	}
+
+	plans := make([]models.Plan, len(snapshots))
+	for i, snapshot := range snapshots {
+		var planEntity entity.PlanEntity
+		if err = snapshot.DataTo(&planEntity); err != nil {
+			return nil, fmt.Errorf("error while converting snapshot to plan entity: %v", err)
+		}
+
+		plan := entity.FromPlanEntity(planEntity)
+		plans[i] = plan
+	}
+
+	return plans, nil
+}
+
 func (p *PlanRepository) collection() *firestore.CollectionRef {
 	return p.client.Collection(collectionPlans)
 }
