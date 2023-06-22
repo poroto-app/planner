02b3973c9e6cf0de6a392f8e9458dfa336c5afe6
0re2236 <2236takahashi@gmail.com>
2023-06-22 14:31:42 +0900

    fix entity and models

--- internal/domain/models/place.go
+++ internal/domain/models/place.go
@@ -3,6 +3,7 @@ package models
 // Place 場所の情報
 // Thumbnail サムネイル用の低画質な写真
 type Place struct {
+	Id                    string      `json:"id"`
 	Name                  string      `json:"name"`
 	Location              GeoLocation `json:"location"`
 	Thumbnail             *string     `json:"thumbnail"`
