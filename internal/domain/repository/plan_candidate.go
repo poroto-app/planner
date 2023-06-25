6270541d88de054dad8703be3e84e618b06e6b70
0re2236 <2236takahashi@gmail.com>
2023-06-25 12:25:24 +0900

    candidateRepositoryにUpdate関数のインターフェースを定義

--- internal/domain/repository/plan_candidate.go
+++ internal/domain/repository/plan_candidate.go
@@ -9,4 +9,5 @@ import (
 type PlanCandidateRepository interface {
 	Save(cxt context.Context, planCandidate *models.PlanCandidate) error
 	Find(ctx context.Context, planCandidateId string) (*models.PlanCandidate, error)
+	UpdatePlacesOrder(ctx context.Context, planId string, planCandidate *models.PlanCandidate, placeIdsOrdered []string) (*models.Plan, error)
 }
