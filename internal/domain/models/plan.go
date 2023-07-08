dd07733d169ea22d62829587c3f088d4a47c03a1
zacker <robomasaki2@gmail.com>
2023-07-08 22:24:28 +0900

    refactor(domain): 移動情報を取得するメソッドを削除

--- internal/domain/models/plan.go
+++ internal/domain/models/plan.go
@@ -1,7 +1,5 @@
 package models
 
-import "fmt"
-
 type Plan struct {
 	Id            string       `json:"id"`
 	Name          string       `json:"name"`
@@ -18,33 +16,4 @@ func (p Plan) GetPlace(placeId string) *Place {
 		}
 	}
 	return nil
-}
-
-// GetTransition 指定した場所から次の場所への移動情報を取得する
-func (p Plan) GetTransition(startPlaceId string) (nextPlace *Place, duration uint, err error) {
-	placeStart := p.GetPlace(startPlaceId)
-	if placeStart == nil {
-		err = fmt.Errorf("could not find place %s in plan", startPlaceId)
-		return
-	}
-
-	var transition *Transition
-	for _, t := range p.Transitions {
-		if t.FromPlaceId == startPlaceId {
-			transition = &t
-			break
-		}
-	}
-	if transition == nil {
-		err = fmt.Errorf("could not find transition from %s in plan", startPlaceId)
-		return
-	}
-
-	nextPlace = p.GetPlace(transition.ToPlaceId)
-	if nextPlace == nil {
-		err = fmt.Errorf("could not find place %s in plan", transition.ToPlaceId)
-		return
-	}
-
-	return nextPlace, transition.Duration, nil
 }
