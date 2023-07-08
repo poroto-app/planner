02c23e70aad70cabedf78db96aa019aae1938aa9
zacker <robomasaki2@gmail.com>
2023-07-08 22:13:40 +0900

    feat(graphql): GraphQLのモデルがTransitionの情報が持つようにする

--- graphql/factory/plan.go
+++ graphql/factory/plan.go
@@ -1,6 +1,8 @@
 package factory
 
 import (
+	"log"
+
 	graphql "poroto.app/poroto/planner/graphql/model"
 	"poroto.app/poroto/planner/internal/domain/models"
 )
@@ -19,21 +21,33 @@ func PlansFromDomainModel(plans *[]models.Plan) []*graphql.Plan {
 func PlanFromDomainModel(plan models.Plan) graphql.Plan {
 	places := make([]*graphql.Place, len(plan.Places))
 	for i, place := range plan.Places {
-		places[i] = &graphql.Place{
-			ID:     place.Id,
-			Name:   place.Name,
-			Photos: place.Photos,
-			Location: &graphql.GeoLocation{
-				Latitude:  place.Location.Latitude,
-				Longitude: place.Location.Longitude,
-			},
-			EstimatedStayDuration: int(place.EstimatedStayDuration),
-		}
+		places[i] = PlaceFromDomainModel(place)
 	}
+
+	transitions := make([]*graphql.Transition, len(plan.Transitions))
+	for _, place := range plan.Places {
+		nextPlace, duration, err := plan.GetTransition(place.Id)
+		if err != nil {
+			log.Println("error while getting transition: ", err)
+			continue
+		}
+
+		if nextPlace == nil {
+			continue
+		}
+
+		transitions = append(transitions, &graphql.Transition{
+			From:     PlaceFromDomainModel(place),
+			To:       PlaceFromDomainModel(*nextPlace),
+			Duration: int(duration),
+		})
+	}
+
 	return graphql.Plan{
 		ID:            plan.Id,
 		Name:          plan.Name,
 		Places:        places,
 		TimeInMinutes: int(plan.TimeInMinutes),
+		Transitions:   transitions,
 	}
 }
