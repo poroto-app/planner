088fd028b49e951885f3967c152cb3727afce619
zacker <robomasaki2@gmail.com>
2023-07-10 20:27:39 +0900

    feat(api): 複数の出力候補を生成できるようにする

--- internal/domain/services/plan/title.go
+++ internal/domain/services/plan/title.go
@@ -16,18 +16,23 @@ func (s PlanService) GeneratePlanTitle(places []models.Place) (*string, error) {
 		placeNames[i] = fmt.Sprintf("%s(%s)", place.Name, place.Category)
 	}
 
-	response, err := s.openaiChatCompletionClient.Complete([]openai.ChatCompletionMessage{
-		{
-			Role: "system",
-			Content: "あなたはコピーライトを生成するアシスタントです" +
-				"例：図書館とカフェを含むプラン" +
-				"生成するコピーライト：新しい本を買って、カフェでゆっくり読書" +
-				"要件：15文字以内の文章になっていること。　体験を想像させるような文章であること",
+	nGenerate := 3
+	response, err := s.openaiChatCompletionClient.Complete(openai.ChatCompletionRequest{
+		Model: openai.ModelGPT3Turbo,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role: "system",
+				Content: "あなたはコピーライトを生成するアシスタントです" +
+					"例：図書館とカフェを含むプラン" +
+					"生成するコピーライト：新しい本を買って、カフェでゆっくり読書" +
+					"要件：15文字以内の文章になっていること。　体験を想像させるような文章であること",
+			},
+			{
+				Role:    "system",
+				Content: fmt.Sprintf("%sを含むプラン", strings.Join(placeNames, "と")),
+			},
 		},
-		{
-			Role:    "user",
-			Content: fmt.Sprintf("%sを含むプラン", strings.Join(placeNames, "と")),
-		},
+		N: &nGenerate,
 	})
 
 	if err != nil {
@@ -38,11 +43,38 @@ func (s PlanService) GeneratePlanTitle(places []models.Place) (*string, error) {
 		return nil, fmt.Errorf("response.Choices is empty")
 	}
 
-	title := response.Choices[0].Message.Content
+	choices := filterByMessageLength(response.Choices, 15)
+	if len(choices) == 0 {
+		return nil, fmt.Errorf("response.Choices is empty")
+	}
+
+	title := choices[indexOfMaxMessageLength(choices)].Content
 	replaceCharacters := []string{"\n", "「", "」", "\""}
 	for _, character := range replaceCharacters {
 		title = strings.ReplaceAll(title, character, "")
 	}
 
 	return &response.Choices[0].Message.Content, nil
+}
+
+func filterByMessageLength(messages []openai.ChatCompletionChoice, length int) []openai.ChatCompletionMessage {
+	filteredMessages := make([]openai.ChatCompletionMessage, 0)
+	for _, message := range messages {
+		if len(message.Message.Content) <= length {
+			filteredMessages = append(filteredMessages, message.Message)
+		}
+	}
+	return filteredMessages
+}
+
+func indexOfMaxMessageLength(messages []openai.ChatCompletionMessage) int {
+	maxLength := 0
+	indexOfMaxLength := 0
+	for i, message := range messages {
+		if len(message.Content) > maxLength {
+			maxLength = len(message.Content)
+			indexOfMaxLength = i
+		}
+	}
+	return indexOfMaxLength
 }
