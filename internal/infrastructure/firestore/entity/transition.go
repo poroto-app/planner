d1e155fe2adcb20daf5c8cf89531d61f93ecbcaa
zacker <robomasaki2@gmail.com>
2023-07-15 16:08:28 +0900

    fix(data): PlanEntityのプロパティをpublicにする

--- internal/infrastructure/firestore/entity/transition.go
+++ internal/infrastructure/firestore/entity/transition.go
@@ -3,18 +3,18 @@ package entity
 import "poroto.app/poroto/planner/internal/domain/models"
 
 type TransitionsEntity struct {
-	from     *string `firestore:"from,omitempty"`
-	to       string  `firestore:"to"`
-	duration int     `firestore:"duration"`
+	FromPlaceId *string `firestore:"from,omitempty"`
+	ToPlaceId   string  `firestore:"to"`
+	Duration    int     `firestore:"duration"`
 }
 
 func ToTransitionsEntities(transitions []models.Transition) *[]TransitionsEntity {
 	ts := make([]TransitionsEntity, len(transitions))
 	for i, transition := range transitions {
 		ts[i] = TransitionsEntity{
-			from:     transition.FromPlaceId,
-			to:       transition.ToPlaceId,
-			duration: int(transition.Duration),
+			FromPlaceId: transition.FromPlaceId,
+			ToPlaceId:   transition.ToPlaceId,
+			Duration:    int(transition.Duration),
 		}
 	}
 	return &ts
@@ -28,9 +28,9 @@ func FromTransitionEntities(entities *[]TransitionsEntity) []models.Transition {
 	ts := make([]models.Transition, len(*entities))
 	for i, entity := range *entities {
 		ts[i] = models.Transition{
-			FromPlaceId: entity.from,
-			ToPlaceId:   entity.to,
-			Duration:    uint(entity.duration),
+			FromPlaceId: entity.FromPlaceId,
+			ToPlaceId:   entity.ToPlaceId,
+			Duration:    uint(entity.Duration),
 		}
 	}
 	return ts
