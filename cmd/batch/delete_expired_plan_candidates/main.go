b4640c52fe97a82f72638702ab52cb5bc966dba0
zacker <robomasaki2@gmail.com>
2023-08-08 15:54:05 +0900

    feat(domain): 有効期限切れのプラン候補を削除するバッチタスクを作成

---
+++ cmd/batch/delete_expired_plan_candidates/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"poroto.app/poroto/planner/internal/domain/services/plancandidate"
+)
+
+// 有効期限切れのプラン候補を削除する
+func main() {
+	log.Printf("=================== Start deleting expired plan candidates ===================\n")
+
+	ctx := context.Background()
+	service, err := plancandidate.NewService(ctx)
+	if err != nil {
+		log.Fatalf("error while initializing plan candidate service: %v", err)
+	}
+
+	if err := service.DeleteExpiredPlanCandidates(ctx); err != nil {
+		log.Fatalf("error while deleting expired plan candidates: %v", err)
+	}
+
+	log.Printf("=================== End deleting expired plan candidates ===================\n")
+}
