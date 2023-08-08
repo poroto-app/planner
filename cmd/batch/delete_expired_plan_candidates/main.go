2b715eee8c7ffd77398f29ba2ff9000851da4258
zacker <robomasaki2@gmail.com>
2023-08-08 17:04:25 +0900

    feat(domain): 環境変数の初期化処理を追加

--- cmd/batch/delete_expired_plan_candidates/main.go
+++ cmd/batch/delete_expired_plan_candidates/main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"poroto.app/poroto/planner/internal/domain/services/plancandidate"
 )
+
+func init() {
+	env := os.Getenv("ENV")
+	if "" == env {
+		env = "development"
+	}
+
+	if err := godotenv.Load(".env.local"); err != nil {
+		log.Fatalf("error while loading .env.local: %v", err)
+	}
+
+	if err := godotenv.Load(".env." + env); err != nil {
+		log.Fatalf("error while loading .env.%s: %v", env, err)
+	}
+}
 
 // 有効期限切れのプラン候補を削除する
 func main() {
