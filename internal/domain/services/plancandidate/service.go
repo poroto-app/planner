8609e3ca9f7f1047331436854fb148481d38cfce
zacker <robomasaki2@gmail.com>
2023-08-08 15:53:16 +0900

    feat(domain): Serviceを初期化する関数を作成

--- internal/domain/services/plancandidate/service.go
+++ internal/domain/services/plancandidate/service.go
@@ -1,8 +1,30 @@
 package plancandidate
 
-import "poroto.app/poroto/planner/internal/domain/repository"
+import (
+	"context"
+
+	"poroto.app/poroto/planner/internal/domain/repository"
+	"poroto.app/poroto/planner/internal/infrastructure/firestore"
+)
 
 type Service struct {
 	planCandidateRepository     repository.PlanCandidateRepository
 	placeSearchResultRepository repository.PlaceSearchResultRepository
+}
+
+func NewService(ctx context.Context) (*Service, error) {
+	planCandidateRepository, err := firestore.NewPlanCandidateRepository(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	placeSearchResultRepository, err := firestore.NewPlaceSearchResultRepository(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Service{
+		planCandidateRepository:     planCandidateRepository,
+		placeSearchResultRepository: placeSearchResultRepository,
+	}, nil
 }
