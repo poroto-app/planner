1ef7c657a3f25e3a831c355ce4d614ea70c6a21e
zacker <robomasaki2@gmail.com>
2023-08-16 10:02:54 +0900

    feat(domain): places apiを追加

--- internal/domain/services/plancandidate/service.go
+++ internal/domain/services/plancandidate/service.go
@@ -2,17 +2,25 @@ package plancandidate
 
 import (
 	"context"
+	"fmt"
 
 	"poroto.app/poroto/planner/internal/domain/repository"
+	"poroto.app/poroto/planner/internal/infrastructure/api/google/places"
 	"poroto.app/poroto/planner/internal/infrastructure/firestore"
 )
 
 type Service struct {
+	placesApi                   places.PlacesApi
 	planCandidateRepository     repository.PlanCandidateRepository
 	placeSearchResultRepository repository.PlaceSearchResultRepository
 }
 
 func NewService(ctx context.Context) (*Service, error) {
+	placesApi, err := places.NewPlacesApi()
+	if err != nil {
+		return nil, fmt.Errorf("error while initializing places api: %v", err)
+	}
+
 	planCandidateRepository, err := firestore.NewPlanCandidateRepository(ctx)
 	if err != nil {
 		return nil, err
@@ -24,6 +32,7 @@ func NewService(ctx context.Context) (*Service, error) {
 	}
 
 	return &Service{
+		placesApi:                   *placesApi,
 		planCandidateRepository:     planCandidateRepository,
 		placeSearchResultRepository: placeSearchResultRepository,
 	}, nil
