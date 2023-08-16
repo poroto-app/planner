ab079e08c680726997b1bbda09d21b3deab72025
zacker <robomasaki2@gmail.com>
2023-08-16 10:31:17 +0900

    refactor(domain): 指定された場所からプランを作成する処理を`PlanGenService`に持たせる

--- internal/domain/services/plan/plan.go
+++ internal/domain/services/plan/plan.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"poroto.app/poroto/planner/internal/domain/models"
 	"poroto.app/poroto/planner/internal/domain/repository"
 	"poroto.app/poroto/planner/internal/domain/services/plangen"
 	"poroto.app/poroto/planner/internal/infrastructure/api/google/places"
@@ -52,52 +51,4 @@ func NewPlanService(ctx context.Context) (*PlanService, error) {
 		placeSearchResultRepository: placeSearchResultRepository,
 		planGeneratorService:        *planGeneratorService,
 	}, err
-}
-
-func (s PlanService) CreatePlanFromPlace(
-	ctx context.Context,
-	createPlanSessionId string,
-	placeId string,
-) (*models.Plan, error) {
-	planCandidate, err := s.planCandidateRepository.Find(ctx, createPlanSessionId)
-	if err != nil {
-		return nil, fmt.Errorf("error while fetching plan candidate")
-	}
-
-	// TODO: ユーザーの興味等を保存しておいて、それを反映させる
-	placesSearched, err := s.placeSearchResultRepository.Find(ctx, createPlanSessionId)
-	if err != nil {
-		return nil, err
-	}
-
-	var placeStart *places.Place
-	for _, place := range placesSearched {
-		if place.PlaceID == placeId {
-			placeStart = &place
-			break
-		}
-	}
-
-	if placeStart == nil {
-		return nil, fmt.Errorf("place not found")
-	}
-
-	planCreated, err := s.planGeneratorService.CreatePlan(
-		ctx,
-		placeStart.Location.ToGeoLocation(),
-		*placeStart,
-		placesSearched,
-		// TODO: freeTimeの項目を保存し、それを反映させる
-		nil,
-		planCandidate.CreatedBasedOnCurrentLocation,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	if _, err = s.planCandidateRepository.AddPlan(ctx, createPlanSessionId, planCreated); err != nil {
-		return nil, err
-	}
-
-	return planCreated, nil
 }
