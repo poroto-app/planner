6f2afe758a635cc4d663f2eeff0f779b5328940f
0re2236 <2236takahashi@gmail.com>
2023-08-20 18:53:33 +0900

    移動情報を更新する関数の実装

--- internal/domain/models/plan.go
+++ internal/domain/models/plan.go
@@ -17,3 +17,38 @@ func (p Plan) GetPlace(placeId string) *Place {
 	}
 	return nil
 }
+
+// 移動情報を更新する（プラン内の場所の順番入れ替えなどの後に用いる）
+func (p Plan) RecreateTransition(startLocation *GeoLocation) []Transition {
+	var firstFromPlaceId *string
+	var firstFromPlaceLocation *GeoLocation
+	var firstToPlaceId int
+
+	// 現在位置から作成されたプラン or 場所指定で作成されたプラン
+	if startLocation != nil {
+		firstFromPlaceId = nil
+		firstFromPlaceLocation = startLocation
+		firstToPlaceId = 0
+	} else {
+		firstFromPlaceId = &p.Places[0].Id
+		firstFromPlaceLocation = &p.Places[0].Location
+		firstToPlaceId = 1
+	}
+	transitions := make([]Transition, len(p.Places)-firstToPlaceId)
+
+	transitions[0] = Transition{
+		FromPlaceId: firstFromPlaceId,
+		ToPlaceId:   p.Places[firstToPlaceId].Id,
+		Duration:    firstFromPlaceLocation.TravelTimeTo(p.Places[firstToPlaceId].Location, 80.0),
+	}
+
+	for i := 0; i < len(p.Places)-firstToPlaceId-1; i++ {
+		transitions[i+1] = Transition{
+			FromPlaceId: &p.Places[firstToPlaceId+i].Id,
+			ToPlaceId:   p.Places[firstToPlaceId+i+1].Id,
+			Duration:    p.Places[firstToPlaceId+i].Location.TravelTimeTo(p.Places[firstToPlaceId+i+1].Location, 80.0),
+		}
+	}
+
+	return transitions
+}
