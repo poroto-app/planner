0bf6fcecc16211cd4c3f5cc8c71c5e39c9009f58
0re2236 <2236takahashi@gmail.com>
2023-08-21 00:33:37 +0900

    再計算処理を一時変数をなるべく使わないよう修正

--- internal/domain/models/plan.go
+++ internal/domain/models/plan.go
@@ -21,35 +21,38 @@ func (p Plan) GetPlace(placeId string) *Place {
 // 移動情報を更新する（プラン内の場所の順番入れ替えなどの後に用いる）
 // startLocation は現在地の座標を表す
 func (p Plan) RecreateTransition(startLocation *GeoLocation) []Transition {
-	var firstFromPlaceId *string
-	var firstFromPlaceLocation *GeoLocation
-	var firstToPlaceIndex int
+	transitions := make([]Transition, 0)
 
 	// 現在位置から作成されたプラン or 場所指定で作成されたプラン
 	if startLocation != nil {
-		firstFromPlaceId = nil
-		firstFromPlaceLocation = startLocation
-		firstToPlaceIndex = 0
+		transitions = append(transitions, Transition{
+			FromPlaceId: nil,
+			ToPlaceId:   p.Places[0].Id,
+			Duration:    startLocation.TravelTimeTo(p.Places[0].Location, 80.0),
+		})
+
+		for i, place := range p.Places {
+			if i >= len(p.Places)-1 {
+				break
+			}
+
+			transitions = append(transitions, Transition{
+				FromPlaceId: &p.Places[i].Id,
+				ToPlaceId:   p.Places[i+1].Id,
+				Duration:    place.Location.TravelTimeTo(p.Places[i+1].Location, 80.0),
+			})
+		}
 	} else {
-		firstFromPlaceId = &p.Places[0].Id
-		firstFromPlaceLocation = &p.Places[0].Location
-		firstToPlaceIndex = 1
-	}
-	transitions := make([]Transition, len(p.Places)-firstToPlaceIndex)
-
-	transitions[0] = Transition{
-		FromPlaceId: firstFromPlaceId,
-		ToPlaceId:   p.Places[firstToPlaceIndex].Id,
-		Duration:    firstFromPlaceLocation.TravelTimeTo(p.Places[firstToPlaceIndex].Location, 80.0),
-	}
-
-	for i := 0; i < len(p.Places)-firstToPlaceIndex-1; i++ {
-		transitions[i+1] = Transition{
-			FromPlaceId: &p.Places[firstToPlaceIndex+i].Id,
-			ToPlaceId:   p.Places[firstToPlaceIndex+i+1].Id,
-			Duration:    p.Places[firstToPlaceIndex+i].Location.TravelTimeTo(p.Places[firstToPlaceIndex+i+1].Location, 80.0),
+		for i, place := range p.Places {
+			if i >= len(p.Places)-1 {
+				break
+			}
+			transitions = append(transitions, Transition{
+				FromPlaceId: &p.Places[i].Id,
+				ToPlaceId:   p.Places[i+1].Id,
+				Duration:    place.Location.TravelTimeTo(p.Places[i+1].Location, 80.0),
+			})
 		}
 	}
-
 	return transitions
 }
