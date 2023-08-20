8dff7dd7e3cf69d36e7c57fa229064c0f56d2024
0re2236 <2236takahashi@gmail.com>
2023-08-21 00:59:16 +0900

    Entityからドメインモデルに変換する際にID配列に従った順序に並び替える処理を実装

--- internal/infrastructure/firestore/entity/plan_in_candidate.go
+++ internal/infrastructure/firestore/entity/plan_in_candidate.go
@@ -40,13 +40,23 @@ func fromPlanInCandidateEntity(
 	name string,
 	places []PlaceEntity,
 	timeInMinutes int,
+	placeIdsOrdered []string,
 	transitions *[]TransitionsEntity,
 ) models.Plan {
-	return fromPlanEntity(
-		id,
-		name,
-		places,
-		timeInMinutes,
-		transitions,
-	)
+	placesOrdered := make([]models.Place, len(places))
+	for i, placeIdOrdered := range placeIdsOrdered {
+		for _, place := range places {
+			if place.Id == placeIdOrdered {
+				placesOrdered[i] = FromPlaceEntity(place)
+			}
+		}
+	}
+
+	return models.Plan{
+		Id:            id,
+		Name:          name,
+		Places:        placesOrdered,
+		TimeInMinutes: uint(timeInMinutes),
+		Transitions:   FromTransitionEntities(transitions),
+	}
 }
