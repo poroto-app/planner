c34de7e45d0faff63e85c89de41551a1348b7700
0re2236 <2236takahashi@gmail.com>
2023-08-22 01:00:25 +0900

    テスト関数にエラー発生時のケースを追加

--- internal/infrastructure/firestore/entity/plan_in_candidate_test.go
+++ internal/infrastructure/firestore/entity/plan_in_candidate_test.go
@@ -15,9 +15,91 @@ func TestFromPlanInCandidateEntity(t *testing.T) {
 		expected []models.Place
 	}{
 		{
+			name: "順序指定ID配列に重複がある場合は場所一覧をプラン作成時の順序でドメインモデルを作成",
+			entity: PlanInCandidateEntity{
+				Id:   "duplication",
+				Name: "プラン候補A",
+				Places: []PlaceEntity{
+					{
+						Id: "01",
+					},
+					{
+						Id: "02",
+					},
+				},
+				PlaceIdsOrdered: []string{
+					"01",
+					"01",
+				},
+			},
+			expected: []models.Place{
+				{
+					Id: "01",
+				},
+				{
+					Id: "02",
+				},
+			},
+		},
+		{
+			name: "順序指定ID配列と場所一覧の示す場所が一致しない場合はプラン作成時の順序でドメインモデルを作成",
+			entity: PlanInCandidateEntity{
+				Id:   "inconsistent",
+				Name: "プラン候補A",
+				Places: []PlaceEntity{
+					{
+						Id: "01",
+					},
+					{
+						Id: "02",
+					},
+				},
+				PlaceIdsOrdered: []string{
+					"10",
+					"20",
+				},
+			},
+			expected: []models.Place{
+				{
+					Id: "01",
+				},
+				{
+					Id: "02",
+				},
+			},
+		},
+		{
+			name: "順序指定ID配列と場所一覧の数が合わない場合はプラン作成時の順序でドメインモデルを作成",
+			entity: PlanInCandidateEntity{
+				Id:   "over_ids",
+				Name: "プラン候補A",
+				Places: []PlaceEntity{
+					{
+						Id: "01",
+					},
+					{
+						Id: "02",
+					},
+				},
+				PlaceIdsOrdered: []string{
+					"01",
+					"02",
+					"03",
+				},
+			},
+			expected: []models.Place{
+				{
+					Id: "01",
+				},
+				{
+					Id: "02",
+				},
+			},
+		},
+		{
 			name: "プラン作成時から場所一覧の順序が並び替えられたケース",
 			entity: PlanInCandidateEntity{
-				Id:   "A",
+				Id:   "correct",
 				Name: "プラン候補A",
 				Places: []PlaceEntity{
 					{
