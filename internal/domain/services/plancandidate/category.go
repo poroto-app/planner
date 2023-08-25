77a6f522f11cd6aa96f1c32854831776b587f478
zacker <robomasaki2@gmail.com>
2023-08-25 21:28:50 +0900

    feat(domain): カテゴリ検索時に追加のカテゴリの場所も検索されるようにする

--- internal/domain/services/plancandidate/category.go
+++ internal/domain/services/plancandidate/category.go
@@ -11,20 +11,13 @@ import (
 	"poroto.app/poroto/planner/internal/infrastructure/api/google/places"
 )
 
+// TODO: PlanGeneratorServiceに持っていく
 func (s Service) CategoriesNearLocation(
 	ctx context.Context,
 	location models.GeoLocation,
 	createPlanSessionId string,
 ) ([]models.LocationCategory, error) {
-	placesSearched, err := s.placesApi.FindPlacesFromLocation(ctx, &places.FindPlacesFromLocationRequest{
-		Location: places.Location{
-			Latitude:  location.Latitude,
-			Longitude: location.Longitude,
-		},
-		Radius:      2000,
-		Language:    "ja",
-		SearchCount: 3,
-	})
+	placesSearched, err := s.planGeneratorService.SearchNearbyPlaces(ctx, location)
 	if err != nil {
 		return nil, fmt.Errorf("error while fetching places: %v\n", err)
 	}
