99c1d287b569c9e97f1d00ed21da2d2d72a62e47
zacker <robomasaki2@gmail.com>
2023-08-28 15:26:17 +0900

    feat(data): author idに紐づくPlanを取得できるようにする

--- internal/domain/repository/plan.go
+++ internal/domain/repository/plan.go
@@ -10,6 +10,7 @@ type PlanRepository interface {
 	Save(ctx context.Context, plan *models.Plan) error
 	SortedByCreatedAt(ctx context.Context, queryCursor *string, limit int) (*[]models.Plan, error)
 	Find(ctx context.Context, planId string) (*models.Plan, error)
+	FindByAuthorId(ctx context.Context, authorId string) (*[]models.Plan, error)
 	// SortedByLocation location で指定した地点に近いプランを返す
 	SortedByLocation(ctx context.Context, location models.GeoLocation, queryCursor *string, limit int) (*[]models.Plan, *string, error)
 }
