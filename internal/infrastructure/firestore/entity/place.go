f7aebf7c5a7650ec63b7c714ae6ba73430678e41
zacker <robomasaki2@gmail.com>
2023-09-17 16:38:22 +0900

    feat(domain): firestoreにレビュー情報が保存されるようにする

--- internal/infrastructure/firestore/entity/place.go
+++ internal/infrastructure/firestore/entity/place.go
@@ -2,18 +2,27 @@ package entity
 
 import "poroto.app/poroto/planner/internal/domain/models"
 
+// PlaceEntity
+// EstimatedStayDuration Firestoreではuintをサポートしていないため，intにしている
 type PlaceEntity struct {
-	Id            string            `firestore:"id"`
-	GooglePlaceId *string           `firestore:"google_place_id"`
-	Name          string            `firestore:"name"`
-	Location      GeoLocationEntity `firestore:"location"`
-	Thumbnail     *string           `firestore:"thumbnail"`
-	Photos        []string          `firestore:"photos"`
-	// MEMO: Firestoreではuintをサポートしていないため，intにしている
-	EstimatedStayDuration int `firestore:"estimated_stay_duration"`
+	Id                    string                     `firestore:"id"`
+	GooglePlaceId         *string                    `firestore:"google_place_id"`
+	Name                  string                     `firestore:"name"`
+	Location              GeoLocationEntity          `firestore:"location"`
+	Thumbnail             *string                    `firestore:"thumbnail"`
+	Photos                []string                   `firestore:"photos"`
+	EstimatedStayDuration int                        `firestore:"estimated_stay_duration"`
+	GooglePlaceReviews    *[]GooglePlaceReviewEntity `firestore:"google_place_reviews,omitempty"`
 }
 
 func ToPlaceEntity(place models.Place) PlaceEntity {
+	var googlePlaceReviews []GooglePlaceReviewEntity
+	if place.GooglePlaceReviews != nil {
+		for _, review := range *place.GooglePlaceReviews {
+			googlePlaceReviews = append(googlePlaceReviews, ToGooglePlaceReviewEntity(review))
+		}
+	}
+
 	return PlaceEntity{
 		Id:                    place.Id,
 		GooglePlaceId:         place.GooglePlaceId,
@@ -22,10 +31,18 @@ func ToPlaceEntity(place models.Place) PlaceEntity {
 		Thumbnail:             place.Thumbnail,
 		Photos:                place.Photos,
 		EstimatedStayDuration: int(place.EstimatedStayDuration),
+		GooglePlaceReviews:    &googlePlaceReviews,
 	}
 }
 
 func FromPlaceEntity(entity PlaceEntity) models.Place {
+	var googlePlaceReviews []models.GooglePlaceReview
+	if entity.GooglePlaceReviews != nil {
+		for _, review := range *entity.GooglePlaceReviews {
+			googlePlaceReviews = append(googlePlaceReviews, FromGooglePlaceReviewEntity(review))
+		}
+	}
+
 	return models.Place{
 		Id:                    entity.Id,
 		GooglePlaceId:         entity.GooglePlaceId,
@@ -34,5 +51,6 @@ func FromPlaceEntity(entity PlaceEntity) models.Place {
 		Thumbnail:             entity.Thumbnail,
 		Photos:                entity.Photos,
 		EstimatedStayDuration: uint(entity.EstimatedStayDuration),
+		GooglePlaceReviews:    &googlePlaceReviews,
 	}
 }
