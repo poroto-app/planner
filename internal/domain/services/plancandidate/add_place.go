e77c114b486ead54dccd2cede503eb4352fad107
zacker <robomasaki2@gmail.com>
2023-10-07 16:52:50 +0900

    feat(domain): ログを追加

--- internal/domain/services/plancandidate/add_place.go
+++ internal/domain/services/plancandidate/add_place.go
@@ -3,14 +3,17 @@ package plancandidate
 import (
 	"context"
 	"fmt"
+	"log"
 	"poroto.app/poroto/planner/internal/domain/models"
 )
 
 func (s Service) AddPlace(ctx context.Context, planCandidateId string, planId string, placeId string) (*models.Plan, error) {
+	log.Printf("Fetching searched places for plan candidate: %v\n", planCandidateId)
 	placesSearched, err := s.placeSearchResultRepository.Find(ctx, planCandidateId)
 	if err != nil {
 		return nil, err
 	}
+	log.Printf("Successfully fetched searched places for plan candidate: %v\n", planCandidateId)
 
 	// 追加する場所を取得する
 	var placeToAdd *models.Place
@@ -26,6 +29,7 @@ func (s Service) AddPlace(ctx context.Context, planCandidateId string, planId st
 
 			placeToAdd = &models.Place{
 				Id:                    place.PlaceID,
+				GooglePlaceId:         &place.PlaceID,
 				Name:                  place.Name,
 				Location:              place.Location.ToGeoLocation(),
 				EstimatedStayDuration: estimatedStayDuration,
@@ -41,20 +45,31 @@ func (s Service) AddPlace(ctx context.Context, planCandidateId string, planId st
 
 	// TODO: キャッシュする
 	places := []models.Place{*placeToAdd}
+
+	log.Printf("Fetching photos and reviews for places for plan candidate: %v\n", planCandidateId)
 	places = s.planGeneratorService.FetchPlacesPhotos(ctx, places)
+	log.Printf("Successfully fetched photos for places for plan candidate: %v\n", planCandidateId)
+
+	log.Printf("Fetching reviews for places for plan candidate: %v\n", planCandidateId)
 	places = s.planGeneratorService.FetchReviews(ctx, places)
-	*placeToAdd = places[0]
+	log.Printf("Successfully fetched reviews for places for plan candidate: %v\n", planCandidateId)
+
+	placeToAdd = &places[0]
 
 	// プランに指定された場所を追加
+	log.Printf("Adding place to plan candidate %v\n", planCandidateId)
 	if err := s.planCandidateRepository.AddPlaceToPlan(ctx, planCandidateId, planId, *placeToAdd); err != nil {
 		return nil, fmt.Errorf("error while adding place to plan candidate: %v\n", err)
 	}
+	log.Printf("Successfully added place to plan candidate %v\n", planCandidateId)
 
 	// 最新のプランの情報を取得
+	log.Printf("Fetching plan candidate: %v\n", planCandidateId)
 	planCandidate, err := s.planCandidateRepository.Find(ctx, planCandidateId)
 	if err != nil {
 		return nil, fmt.Errorf("error while fetching plan candidate: %v\n", err)
 	}
+	log.Printf("Successfully fetched plan candidate: %v\n", planCandidateId)
 
 	plan := planCandidate.GetPlan(planId)
 	if plan == nil {
