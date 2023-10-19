86c9ab3428784f04ff1dd48d44c5893112022b32
zacker <iszkmasaki@gmail.com>
2023-10-19 16:23:55 +0900

    fix(domain): pass the pointer of value

--- internal/domain/services/plancandidate/replace_place.go
+++ internal/domain/services/plancandidate/replace_place.go
@@ -3,35 +3,43 @@ package plancandidate
 import (
 	"context"
 	"fmt"
+	"log"
 	"poroto.app/poroto/planner/internal/domain/models"
 )
 
 func (s Service) ReplacePlace(ctx context.Context, planCandidateId string, planId string, placeIdToBeReplaced string, placeIdToReplace string) (*models.Plan, error) {
+	log.Printf("start fetching plan candidate: %v\n", planCandidateId)
 	planCandidate, err := s.planCandidateRepository.Find(ctx, planCandidateId)
 	if err != nil {
 		return nil, fmt.Errorf("error while fetching plan candidate: %v\n", err)
 	}
+	log.Printf("succeeded fetching plan candidate: %v\n", planCandidateId)
 
 	planToUpdate := planCandidate.GetPlan(planId)
 	if planToUpdate == nil {
 		return nil, fmt.Errorf("plan not found: %v\n", planId)
 	}
 
+	log.Printf("start searching places: %v\n", planCandidateId)
 	placesSearched, err := s.placeSearchResultRepository.Find(ctx, planCandidateId)
 	if err != nil {
 		return nil, err
 	}
+	log.Printf("succeeded searching places: %v\n", planCandidateId)
 
+	log.Printf("start fetching place to be replaced: %v\n", placeIdToBeReplaced)
 	placeToBeReplaced := planToUpdate.GetPlace(placeIdToBeReplaced)
 	if placeToBeReplaced == nil {
 		return nil, fmt.Errorf("place to be replaced not found: %v\n", placeIdToBeReplaced)
 	}
+	log.Printf("succeeded fetching place to be replaced: %v\n", placeIdToBeReplaced)
 
 	var placeToReplace *models.Place
 	for _, place := range placesSearched {
 		// TODO: PlaceRepositoryを用いて、Planner APIが指定したPlaceIdで取得できるようにする
 		if place.PlaceId == placeIdToReplace {
-			*placeToReplace = place.ToPlace()
+			p := place.ToPlace()
+			placeToReplace = &p
 			break
 		}
 	}
@@ -39,9 +47,11 @@ func (s Service) ReplacePlace(ctx context.Context, planCandidateId string, planI
 		return nil, fmt.Errorf("place to replace not found: %v\n", placeIdToReplace)
 	}
 
+	log.Printf("start replacing place: %v\n", placeIdToBeReplaced)
 	if err := s.planCandidateRepository.ReplacePlace(ctx, planCandidateId, planId, placeIdToBeReplaced, *placeToReplace); err != nil {
 		return nil, fmt.Errorf("error while replacing place: %v\n", err)
 	}
+	log.Printf("succeeded replacing place: %v\n", placeIdToBeReplaced)
 
 	planCandidateUpdated, err := s.planCandidateRepository.Find(ctx, planCandidateId)
 	if err != nil {
