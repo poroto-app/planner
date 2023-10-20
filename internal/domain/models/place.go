b3e6ca0fc0c93958942f9fc0b59404991a0d2199
0re2236 <2236takahashi@gmail.com>
2023-10-20 15:45:33 +0900

    ToIntPointer関数を汎用メソッド化

--- internal/domain/models/place.go
+++ internal/domain/models/place.go
@@ -1,5 +1,7 @@
 package models
 
+import "poroto.app/poroto/planner/internal/domain/utils"
+
 // Place 場所の情報
 type Place struct {
 	Id                 string               `json:"id"`
@@ -38,17 +40,13 @@ func (p Place) EstimatedStayDuration() uint {
 func (p Place) EstimatedPriceRange() (priceRangeMin, priceRangeMax *int) {
 	switch *p.PriceLevel {
 	case 0:
-		return toIntPointer(limitOfPriceRangeMin), toIntPointer(thresholdOfLevel0AndLevel1_2)
+		return utils.ToIntPointer(limitOfPriceRangeMin), utils.ToIntPointer(thresholdOfLevel0AndLevel1_2)
 	case 1, 2:
-		return toIntPointer(thresholdOfLevel0AndLevel1_2), toIntPointer(thresholdOfLevel1_2AndLevel3)
+		return utils.ToIntPointer(thresholdOfLevel0AndLevel1_2), utils.ToIntPointer(thresholdOfLevel1_2AndLevel3)
 	case 3:
-		return toIntPointer(thresholdOfLevel1_2AndLevel3), toIntPointer(thresholdOfLevel3AndLevel4)
+		return utils.ToIntPointer(thresholdOfLevel1_2AndLevel3), utils.ToIntPointer(thresholdOfLevel3AndLevel4)
 	case 4:
-		return toIntPointer(thresholdOfLevel3AndLevel4), toIntPointer(limitOfPriceRangeMax)
+		return utils.ToIntPointer(thresholdOfLevel3AndLevel4), utils.ToIntPointer(limitOfPriceRangeMax)
 	}
 	return nil, nil
-}
-
-func toIntPointer(x int) *int {
-	return &x
 }
