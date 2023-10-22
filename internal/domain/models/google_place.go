ea5eb942b389d1d5c188c0803b0aeee2ebc8308f
zacker <robomasaki2@gmail.com>
2023-10-22 15:52:52 +0900

    feat(domain): 不要な関数を削除

--- internal/domain/models/google_place.go
+++ internal/domain/models/google_place.go
@@ -1,9 +1,5 @@
 package models
 
-import (
-	"poroto.app/poroto/planner/internal/domain/utils"
-)
-
 type GooglePlace struct {
 	PlaceId          string
 	Name             string
@@ -15,31 +11,6 @@ type GooglePlace struct {
 	UserRatingsTotal int
 	Images           *[]Image
 	Reviews          *[]GooglePlaceReview
-}
-
-func (g GooglePlace) ToPlace() Place {
-	if g.Images == nil {
-		g.Images = new([]Image)
-	}
-
-	// TODO: planner api が生成したIDと対応させる
-	return Place{
-		Id:                 g.PlaceId,
-		GooglePlaceId:      utils.StrPointer(g.PlaceId),
-		Name:               g.Name,
-		Location:           g.Location,
-		Images:             *g.Images,
-		Categories:         GetCategoriesFromSubCategories(g.Types),
-		GooglePlaceReviews: g.Reviews,
-	}
-}
-
-func (g GooglePlace) EstimatedStayDuration() uint {
-	categories := GetCategoriesFromSubCategories(g.Types)
-	if len(categories) == 0 {
-		return 0
-	}
-	return categories[0].EstimatedStayDuration
 }
 
 // IndexOfCategory は Types 中の `category` に対応する Type のインデックスを返す
