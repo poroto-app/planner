e4f4c600bf21c4224adb887501bdf6937e422a44
zacker <robomasaki2@gmail.com>
2023-11-18 21:41:41 +0900

    feat(data): 開店時間を表す構造体を宣言

---
+++ internal/infrastructure/firestore/entity/google_opening_hours.go
@@ -0,0 +1,44 @@
+package entity
+
+import "poroto.app/poroto/planner/internal/domain/models"
+
+type GooglePlaceOpeningHoursEntity struct {
+	OpeningHoursPeriod []GooglePlaceOpeningPeriodEntity `firestore:"periods"`
+}
+
+type GooglePlaceOpeningPeriodEntity struct {
+	DayOfWeekOpen  string `firestore:"open_day"`
+	DayOfWeekClose string `firestore:"close_day"`
+	TimeOpen       string `firestore:"open_time"`
+	TimeClose      string `firestore:"close_time"`
+}
+
+func GooglePlaceOpeningsEntityFromGooglePlaceOpeningPeriod(periods []models.GooglePlaceOpeningPeriod) GooglePlaceOpeningHoursEntity {
+	var entities []GooglePlaceOpeningPeriodEntity
+	for _, period := range periods {
+		entities = append(entities, GooglePlaceOpeningPeriodEntity{
+			DayOfWeekOpen:  period.DayOfWeekOpen,
+			DayOfWeekClose: period.DayOfWeekClose,
+			TimeOpen:       period.OpeningTime,
+			TimeClose:      period.ClosingTime,
+		})
+	}
+
+	return GooglePlaceOpeningHoursEntity{
+		OpeningHoursPeriod: entities,
+	}
+}
+
+func (g GooglePlaceOpeningHoursEntity) ToGooglePlaceOpeningPeriods() []models.GooglePlaceOpeningPeriod {
+	var periods []models.GooglePlaceOpeningPeriod
+	for _, period := range g.OpeningHoursPeriod {
+		periods = append(periods, models.GooglePlaceOpeningPeriod{
+			DayOfWeekOpen:  period.DayOfWeekOpen,
+			DayOfWeekClose: period.DayOfWeekClose,
+			OpeningTime:    period.TimeOpen,
+			ClosingTime:    period.TimeClose,
+		})
+	}
+
+	return periods
+}
