66b3b70d3d50ee2f7793e917708607057bbc2f43
zacker <robomasaki2@gmail.com>
2023-11-18 15:49:48 +0900

    feat(domain): GooglePlace構造体に開店時間を表すプロパティを追加

--- internal/domain/models/google_place.go
+++ internal/domain/models/google_place.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"fmt"
+	"time"
+)
+
 type GooglePlace struct {
 	PlaceId          string
 	Name             string
@@ -10,6 +15,7 @@ type GooglePlace struct {
 	PriceLevel       int
 	Rating           float32
 	UserRatingsTotal int
+	OpeningHours     *[]GooglePlaceOpeningPeriod
 	Images           *[]Image
 	Reviews          *[]GooglePlaceReview
 }
@@ -23,4 +29,38 @@ func (g GooglePlace) IndexOfCategory(category LocationCategory) int {
 		}
 	}
 	return -1
+}
+
+func (g GooglePlace) IsOpening(at time.Time) (bool, error) {
+	if g.OpeningHours == nil {
+		return false, fmt.Errorf("opening hours is not set")
+	}
+
+	for _, openingPeriod := range *g.OpeningHours {
+		weekday := at.Weekday()
+		isOpeningPeriodOfToday := openingPeriod.DayOfWeekOpen == weekday.String()
+		if !isOpeningPeriodOfToday {
+			continue
+		}
+
+		openingTime, err := openingPeriod.OpeningTimeHHMM()
+		if err != nil {
+			return false, fmt.Errorf("error while parsing opening time")
+		}
+
+		closingHour, err := openingPeriod.ClosingTimeHHMM()
+		if err != nil {
+			return false, fmt.Errorf("error while parsing closing time")
+		}
+
+		today := time.Date(at.Year(), at.Month(), at.Day(), 0, 0, 0, 0, at.Location())
+		openingTimeAtToday := today.Add(time.Hour*time.Duration(openingTime.Hour) + time.Minute*time.Duration(openingTime.Minute))
+		closingTimeAtToday := today.Add(time.Hour*time.Duration(closingHour.Hour) + time.Minute*time.Duration(closingHour.Minute))
+
+		if at.After(openingTimeAtToday) && at.Before(closingTimeAtToday) {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
