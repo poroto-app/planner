1a0093e127b27973b309a65e1c65e29793afed2e
zacker <robomasaki2@gmail.com>
2023-11-19 14:36:49 +0900

    feat(data): 画像データがない場合はnilを返す

--- internal/infrastructure/firestore/entity/google_place_photo.go
+++ internal/infrastructure/firestore/entity/google_place_photo.go
@@ -5,6 +5,8 @@ import (
 	"poroto.app/poroto/planner/internal/domain/utils"
 )
 
+// GooglePlacePhotoEntity 場所の写真
+// models.GooglePlacePhoto だけでなく、 models.GooglePlacePhotoReference としても扱う（画像を取得しているかどうかの差のみのため）
 type GooglePlacePhotoEntity struct {
 	GooglePlaceId    string   `firestore:"google_place_id"`
 	PhotoReference   string   `firestore:"photo_reference"`
@@ -27,13 +29,27 @@ func GooglePlacePhotoEntityFromGooglePlacePhoto(googlePlacePhoto models.GooglePl
 	}
 }
 
-func (g GooglePlacePhotoEntity) ToGooglePlacePhoto() models.GooglePlacePhoto {
-	return models.GooglePlacePhoto{
+func (g GooglePlacePhotoEntity) ToGooglePlacePhoto() *models.GooglePlacePhoto {
+	// SmallもLargeもnilの場合は、画像を取得していないため nil にする
+	if g.Small == nil && g.Large == nil {
+		return nil
+	}
+
+	return &models.GooglePlacePhoto{
 		PhotoReference:   g.PhotoReference,
 		Width:            g.Width,
 		Height:           g.Height,
 		HTMLAttributions: g.HTMLAttributions,
 		Small:            utils.StrCopyPointerValue(g.Small),
 		Large:            utils.StrCopyPointerValue(g.Large),
+	}
+}
+
+func (g GooglePlacePhotoEntity) ToGooglePlacePhotoReference() models.GooglePlacePhotoReference {
+	return models.GooglePlacePhotoReference{
+		PhotoReference:   g.PhotoReference,
+		Width:            g.Width,
+		Height:           g.Height,
+		HTMLAttributions: g.HTMLAttributions,
 	}
 }
