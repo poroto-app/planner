edd322717800f4fe7fa75df54b16132c65a1ee52
zacker <robomasaki2@gmail.com>
2023-11-19 11:10:52 +0900

    feat(data): PhotoReferenceの情報をもった`GooglePlacePhotoEntityを定義`

---
+++ internal/infrastructure/firestore/entity/google_place_photo.go
@@ -0,0 +1,39 @@
+package entity
+
+import (
+	"poroto.app/poroto/planner/internal/domain/models"
+	"poroto.app/poroto/planner/internal/domain/utils"
+)
+
+type GooglePlacePhotoEntity struct {
+	GooglePlaceId    string   `firestore:"google_place_id"`
+	PhotoReference   string   `firestore:"photo_reference"`
+	Width            int      `firestore:"width"`
+	Height           int      `firestore:"height"`
+	HTMLAttributions []string `firestore:"html_attributions"`
+	Small            *string  `firestore:"small,omitempty"`
+	Large            *string  `firestore:"large,omitempty"`
+}
+
+func GooglePlacePhotoEntityFromGooglePlacePhoto(googlePlaceId string, googlePlacePhoto models.GooglePlacePhoto) GooglePlacePhotoEntity {
+	return GooglePlacePhotoEntity{
+		GooglePlaceId:    googlePlaceId,
+		PhotoReference:   googlePlacePhoto.PhotoReference,
+		Width:            googlePlacePhoto.Width,
+		Height:           googlePlacePhoto.Height,
+		HTMLAttributions: googlePlacePhoto.HTMLAttributions,
+		Small:            utils.StrCopyPointerValue(googlePlacePhoto.Small),
+		Large:            utils.StrCopyPointerValue(googlePlacePhoto.Large),
+	}
+}
+
+func (g GooglePlacePhotoEntity) ToGooglePlacePhoto() models.GooglePlacePhoto {
+	return models.GooglePlacePhoto{
+		PhotoReference:   g.PhotoReference,
+		Width:            g.Width,
+		Height:           g.Height,
+		HTMLAttributions: g.HTMLAttributions,
+		Small:            utils.StrCopyPointerValue(g.Small),
+		Large:            utils.StrCopyPointerValue(g.Large),
+	}
+}
