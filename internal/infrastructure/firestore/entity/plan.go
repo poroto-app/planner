e90babe8777d22799ef7b5482701ca23a1e59081
zacker <robomasaki2@gmail.com>
2023-11-26 12:15:12 +0900

    feat(data): PlanEntityにPlaceを持たせない

--- internal/infrastructure/firestore/entity/plan.go
+++ internal/infrastructure/firestore/entity/plan.go
@@ -9,21 +9,15 @@ import (
 // PlanEntity は保存されたプランを示す
 // GeoHash はプランの最初の場所のGeoHashを示す（プランは小さい範囲で作られるため、どこをとってもあまり変わらない）
 type PlanEntity struct {
-	Id        string        `firestore:"id"`
-	Name      string        `firestore:"name"`
-	Places    []PlaceEntity `firestore:"places"`
-	GeoHash   *string       `firestore:"geohash,omitempty"`
-	CreatedAt time.Time     `firestore:"created_at,omitempty,serverTimestamp"`
-	UpdatedAt time.Time     `firestore:"updated_at,omitempty"`
-	AuthorId  *string       `firestore:"author_id,omitempty"`
+	Id        string    `firestore:"id"`
+	Name      string    `firestore:"name"`
+	GeoHash   *string   `firestore:"geohash,omitempty"`
+	CreatedAt time.Time `firestore:"created_at,omitempty,serverTimestamp"`
+	UpdatedAt time.Time `firestore:"updated_at,omitempty"`
+	AuthorId  *string   `firestore:"author_id,omitempty"`
 }
 
 func ToPlanEntity(plan models.Plan) PlanEntity {
-	places := make([]PlaceEntity, len(plan.Places))
-	for i, place := range plan.Places {
-		places[i] = NewPlaceEntityFromPlace(place)
-	}
-
 	var geohash *string
 	if len(plan.Places) > 0 {
 		value := plan.Places[0].Location.GeoHash()
@@ -33,7 +27,6 @@ func ToPlanEntity(plan models.Plan) PlanEntity {
 	return PlanEntity{
 		Id:        plan.Id,
 		Name:      plan.Name,
-		Places:    places,
 		GeoHash:   geohash,
 		AuthorId:  plan.AuthorId,
 		UpdatedAt: time.Now(),
@@ -41,15 +34,9 @@ func ToPlanEntity(plan models.Plan) PlanEntity {
 }
 
 func FromPlanEntity(entity PlanEntity) models.Plan {
-	ps := make([]models.Place, len(entity.Places))
-	for i, placeEntity := range entity.Places {
-		ps[i] = placeEntity.ToPlace()
-	}
-
 	return models.Plan{
 		Id:       entity.Id,
 		Name:     entity.Name,
-		Places:   ps,
 		AuthorId: entity.AuthorId,
 	}
 }
