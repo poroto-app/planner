b0d71c198b0114f72c756221e25aceaa06d64daa
zacker <robomasaki2@gmail.com>
2023-12-09 22:35:27 +0900

    refactor(domain): 不要な関数を削除

--- internal/domain/services/place/place_detail.go
+++ internal/domain/services/place/place_detail.go
@@ -62,60 +62,57 @@ func (s Service) FetchPlaceDetailAndSave(ctx context.Context, googlePlaceId stri
 	return &placeDetail, nil
 }
 
-// FetchGooglePlacesDetailAndSave 複数の場所の Place Detail 情報を並行に取得し、保存する
-func (s Service) FetchGooglePlacesDetailAndSave(ctx context.Context, places []models.GooglePlace) []models.GooglePlace {
+// FetchPlacesDetailAndSave 複数の場所の Place Detail 情報を並行に取得し、保存する
+func (s Service) FetchPlacesDetailAndSave(ctx context.Context, places []models.Place) []models.Place {
 	if len(places) == 0 {
 		return nil
 	}
 
-	ch := make(chan *models.GooglePlace, len(places))
-	for _, place := range places {
-		go func(ctx context.Context, place models.GooglePlace, ch chan<- *models.GooglePlace) {
-			if place.PlaceDetail != nil {
-				s.logger.Info(
-					"skip fetching place detail because place detail already exist",
-					zap.String("placeId", place.PlaceId),
-				)
-				ch <- &place
-				return
-			}
-
-			placeDetail, err := s.FetchPlaceDetailAndSave(ctx, place.PlaceId)
-			if err != nil {
-				ch <- nil
-				return
-			}
-
-			place.PlaceDetail = placeDetail
-
-			ch <- &place
-		}(ctx, place, ch)
-	}
-
-	for i := 0; i < len(places); i++ {
-		placeWithPlaceDetail := <-ch
-		if placeWithPlaceDetail == nil {
-			continue
-		}
-
-		for iPlace, place := range places {
-			if placeWithPlaceDetail.PlaceId == place.PlaceId {
-				places[iPlace] = *placeWithPlaceDetail
-			}
-		}
-	}
-
-	return places
-}
-
-func (s Service) FetchPlacesDetailAndSave(ctx context.Context, places []models.Place) []models.Place {
 	googlePlaces := make([]models.GooglePlace, len(places))
 	for i, place := range places {
 		googlePlaces[i] = place.Google
 	}
 
-	googlePlaces = s.FetchGooglePlacesDetailAndSave(ctx, googlePlaces)
+	// Place Detailを並行に取得する
+	ch := make(chan *models.GooglePlace, len(googlePlaces))
+	for _, googlePlace := range googlePlaces {
+		go func(ctx context.Context, googlePlace models.GooglePlace, ch chan<- *models.GooglePlace) {
+			if googlePlace.PlaceDetail != nil {
+				s.logger.Info(
+					"skip fetching place detail because place detail already exist",
+					zap.String("placeId", googlePlace.PlaceId),
+				)
+				ch <- &googlePlace
+				return
+			}
 
+			// 取得と保存を行う
+			placeDetail, err := s.FetchPlaceDetailAndSave(ctx, googlePlace.PlaceId)
+			if err != nil {
+				ch <- nil
+				return
+			}
+
+			googlePlace.PlaceDetail = placeDetail
+
+			ch <- &googlePlace
+		}(ctx, googlePlace, ch)
+	}
+
+	for i := 0; i < len(googlePlaces); i++ {
+		placeWithPlaceDetail := <-ch
+		if placeWithPlaceDetail == nil {
+			continue
+		}
+
+		for iPlace, googlePlace := range googlePlaces {
+			if placeWithPlaceDetail.PlaceId == googlePlace.PlaceId {
+				googlePlaces[iPlace] = *placeWithPlaceDetail
+			}
+		}
+	}
+
+	// Place DetailとGoogle Placeを紐付ける
 	for i, googlePlace := range googlePlaces {
 		places[i].Google = googlePlace
 	}
