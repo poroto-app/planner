c641b4ccdb8d42bf5ee33b7ca66787da2bd2c1ac
0re2236 <2236takahashi@gmail.com>
2023-12-15 18:30:45 +0900

    Refactor UpdateLikeToPlaceInPlanCandidate function

--- internal/infrastructure/firestore/plan_candidate.go
+++ internal/infrastructure/firestore/plan_candidate.go
@@ -696,8 +696,28 @@ func (p *PlanCandidateFirestoreRepository) UpdateLikeToPlaceInPlanCandidate(ctx 
 			return fmt.Errorf("error while converting snapshot to plan candidate entity: %v", err)
 		}
 
-		// すでにLikeしている場合は、Likeを取り消し
-		if place.LikeCount > 0 && !like && array.IsContain(planCandidateEntity.LikedPlaceIds, place.Id) {
+		if like {
+			if array.IsContain(planCandidateEntity.LikedPlaceIds, place.Id) {
+				p.logger.Info(
+					"mismatching like state between input and DB",
+					zap.Bool("like", like),
+					zap.Bool("IsContain", array.IsContain(planCandidateEntity.LikedPlaceIds, place.Id)),
+				)
+				return nil
+			}
+			// まだLikeされていない場合は、Likeを追加
+			place.LikeCount += 1
+			planCandidateEntity.LikedPlaceIds = append(planCandidateEntity.LikedPlaceIds, place.Id)
+		} else {
+			if !array.IsContain(planCandidateEntity.LikedPlaceIds, place.Id) {
+				p.logger.Info(
+					"mismatching like state between input and DB",
+					zap.Bool("like", like),
+					zap.Bool("IsContain", array.IsContain(planCandidateEntity.LikedPlaceIds, place.Id)),
+				)
+				return nil
+			}
+			// すでにLikeしている場合は、Likeを取り消し
 			place.LikeCount -= 1
 			for i, id := range planCandidateEntity.LikedPlaceIds {
 				if id == place.Id {
@@ -705,17 +725,6 @@ func (p *PlanCandidateFirestoreRepository) UpdateLikeToPlaceInPlanCandidate(ctx 
 					break
 				}
 			}
-		} else if like && !array.IsContain(planCandidateEntity.LikedPlaceIds, place.Id) {
-			// まだLikeされていない場合は、Likeを追加
-			place.LikeCount += 1
-			planCandidateEntity.LikedPlaceIds = append(planCandidateEntity.LikedPlaceIds, place.Id)
-		} else {
-			p.logger.Info(
-				"mismatching like state between input and DB",
-				zap.Bool("like", like),
-				zap.Bool("IsContain", array.IsContain(planCandidateEntity.LikedPlaceIds, place.Id)),
-			)
-			return nil
 		}
 
 		// PlanCandidateを更新する
