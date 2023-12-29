1c65aefd4a77bb6d7d89d57a26dffdca28f437cc
zacker <robomasaki2@gmail.com>
2023-12-29 09:34:11 +0900

    feat(domain): 配列の要素を変形したとき条件を満たすものだけを取得する関数を作成

--- internal/domain/array/transform.go
+++ internal/domain/array/transform.go
@@ -7,3 +7,13 @@ func Map[T any, U any](slice []T, transform func(T) U) []U {
 	}
 	return mapped
 }
+
+func MapAndFilter[T any, U any](slice []T, transform func(T) (U, bool)) []U {
+	var mapped []U
+	for _, v := range slice {
+		if u, ok := transform(v); ok {
+			mapped = append(mapped, u)
+		}
+	}
+	return mapped
+}
--- internal/domain/array/transform_test.go
+++ internal/domain/array/transform_test.go
@@ -46,3 +46,51 @@ func TestMap(t *testing.T) {
 		})
 	}
 }
+
+func TestMapAndFilter(t *testing.T) {
+	cases := []struct {
+		name      string
+		slice     []int
+		transform func(int) (string, bool)
+		expected  []string
+	}{
+		{
+			name:      "empty slice",
+			transform: func(i int) (string, bool) { return strconv.Itoa(i), true },
+			slice:     []int{},
+			expected:  []string{},
+		},
+		{
+			name:      "no match",
+			transform: func(i int) (string, bool) { return strconv.Itoa(i), i < 0 },
+			slice:     []int{1, 2, 3},
+			expected:  []string{},
+		},
+		{
+			name:      "one match",
+			transform: func(i int) (string, bool) { return strconv.Itoa(i), i == 2 },
+			slice:     []int{1, 2, 3},
+			expected:  []string{"2"},
+		},
+		{
+			name:      "multiple matches",
+			transform: func(i int) (string, bool) { return strconv.Itoa(i), i > 1 },
+			slice:     []int{1, 2, 3},
+			expected:  []string{"2", "3"},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := MapAndFilter(c.slice, c.transform)
+			if len(actual) != len(c.expected) {
+				t.Errorf("expected: %v, actual: %v", c.expected, actual)
+			}
+			for i, v := range actual {
+				if v != c.expected[i] {
+					t.Errorf("expected: %v, actual: %v", c.expected, actual)
+				}
+			}
+		})
+	}
+}
