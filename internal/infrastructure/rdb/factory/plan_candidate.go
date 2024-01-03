673e72a3e2283dd8e8e64aefd085b68d98a304b8
zacker <robomasaki2@gmail.com>
2024-01-03 15:11:03 +0900

    refactor(data): 不要なfactory関数を削除

--- internal/infrastructure/rdb/factory/plan_candidate.go
+++ internal/infrastructure/rdb/factory/plan_candidate.go
@@ -5,15 +5,6 @@ import (
 	"poroto.app/poroto/planner/internal/infrastructure/rdb/entities"
 )
 
-func PlanCandidateSliceFromDomainModel(planCandidates []models.Plan, planCandidateSetId string) entities.PlanCandidateSlice {
-	var planCandidateSlice entities.PlanCandidateSlice
-	for _, planCandidate := range planCandidates {
-		planCandidateEntity := PlanCandidateEntityFromDomainModel(planCandidate, planCandidateSetId)
-		planCandidateSlice = append(planCandidateSlice, &planCandidateEntity)
-	}
-	return planCandidateSlice
-}
-
 func PlanCandidateEntityFromDomainModel(planCandidate models.Plan, planCandidateSetId string) entities.PlanCandidate {
 	return entities.PlanCandidate{
 		ID:                 planCandidate.Id,
