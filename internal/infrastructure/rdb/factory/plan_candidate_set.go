a710881351d62342257d502d0059b60f7b7b6a9a
zacker <robomasaki2@gmail.com>
2024-01-04 17:59:02 +0900

    feat(data): MetaDataを取得できなかったときにログを出す

--- internal/infrastructure/rdb/factory/plan_candidate_set.go
+++ internal/infrastructure/rdb/factory/plan_candidate_set.go
@@ -1,7 +1,9 @@
 package factory
 
 import (
+	"go.uber.org/zap"
 	"poroto.app/poroto/planner/internal/domain/models"
+	"poroto.app/poroto/planner/internal/domain/utils"
 	"poroto.app/poroto/planner/internal/infrastructure/rdb/entities"
 )
 
@@ -16,6 +18,13 @@ func NewPlanCandidateSetFromEntity(
 	planCandidateSetMetaData, err := NewPlanCandidateMetaDataFromEntity(planCandidateSetMetaDataSlice, planCandidateSetCategorySlice, planCandidateSetEntity.ID)
 	if err != nil {
 		// PlanCandidateSetMetaDataがない場合はエラーにしない
+		logger, err := utils.NewLogger(utils.LoggerOption{
+			Tag: "NewPlanCandidateSetFromEntity",
+		})
+		if err != nil {
+			return nil, err
+		}
+		logger.Debug("skip to create PlanCandidateSetMetaData", zap.Error(err))
 		planCandidateSetMetaData = &models.PlanCandidateMetaData{}
 	}
 
