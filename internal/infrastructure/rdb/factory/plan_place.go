fbbe484ac5c4b75734bfeed017515b875fd32962
zacker <robomasaki2@gmail.com>
2024-01-06 14:55:07 +0900

    feat(data): プランに含まれるPlaceを変換するfactory関数を作成

--- internal/infrastructure/rdb/factory/plan_place.go
+++ internal/infrastructure/rdb/factory/plan_place.go
@@ -1,8 +1,11 @@
 package factory
 
 import (
+	"fmt"
+	"poroto.app/poroto/planner/internal/domain/array"
 	"poroto.app/poroto/planner/internal/domain/models"
 	"poroto.app/poroto/planner/internal/infrastructure/rdb/generated"
+	"sort"
 )
 
 func NewPlanPlaceSliceFromDomainMode(planPlaces []models.Place, planId string) generated.PlanPlaceSlice {
@@ -16,4 +19,40 @@ func NewPlanPlaceSliceFromDomainMode(planPlaces []models.Place, planId string) g
 		})
 	}
 	return planPlaceSlice
+}
+
+func NewPlanPlacesFromEntities(
+	planPlaceSlice generated.PlanPlaceSlice,
+	places []models.Place,
+	planId string,
+) (*[]models.Place, error) {
+	planPlaceEntities := array.MapAndFilter(planPlaceSlice, func(planPlaceEntity *generated.PlanPlace) (generated.PlanPlace, bool) {
+		if planPlaceEntity == nil {
+			return generated.PlanPlace{}, false
+		}
+		if planPlaceEntity.PlanID != planId {
+			return generated.PlanPlace{}, false
+		}
+		return *planPlaceEntity, true
+	})
+
+	// SortOrder でソート
+	sort.Slice(planPlaceEntities, func(i, j int) bool {
+		return planPlaceEntities[i].SortOrder < planPlaceEntities[j].SortOrder
+	})
+
+	planPlaces, err := array.MapWithErr(planPlaceEntities, func(planPlaceEntity generated.PlanPlace) (*models.Place, error) {
+		place, ok := array.Find(places, func(place models.Place) bool {
+			return place.Id == planPlaceEntity.PlaceID
+		})
+		if !ok {
+			return nil, fmt.Errorf("place not found: %s", planPlaceEntity.PlaceID)
+		}
+		return &place, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return planPlaces, nil
 }
--- internal/infrastructure/rdb/factory/plan_place_test.go
+++ internal/infrastructure/rdb/factory/plan_place_test.go
@@ -1,6 +1,7 @@
 package factory
 
 import (
+	"github.com/google/go-cmp/cmp"
 	"poroto.app/poroto/planner/internal/domain/models"
 	"poroto.app/poroto/planner/internal/infrastructure/rdb/generated"
 	"testing"
@@ -39,6 +40,7 @@ func TestNewPlanPlaceSliceFromDomainModel(t *testing.T) {
 	for _, c := range cases {
 		c := c
 		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
 			actual := NewPlanPlaceSliceFromDomainMode(c.planPlaces, c.planId)
 			if len(actual) != len(c.expected) {
 				t.Errorf("wrong plan place slice length, want: %d, got: %d", len(c.expected), len(actual))
@@ -51,6 +53,102 @@ func TestNewPlanPlaceSliceFromDomainModel(t *testing.T) {
 				if actual[i].SortOrder != expected.SortOrder {
 					t.Errorf("wrong sort order, want: %d, got: %d", expected.SortOrder, actual[i].SortOrder)
 				}
+			}
+		})
+	}
+}
+
+func TestNewPlacesFromEntities(t *testing.T) {
+	cases := []struct {
+		name           string
+		planPlaceSlice generated.PlanPlaceSlice
+		places         []models.Place
+		planId         string
+		expected       []models.Place
+	}{
+		{
+			name:   "should return a valid slice",
+			planId: "4a81310e-03c9-4862-8a13-d3c75475bc6e",
+			planPlaceSlice: generated.PlanPlaceSlice{
+				{
+					PlaceID:   "ec7c607d-454a-4644-929a-c3b1e078842d",
+					PlanID:    "4a81310e-03c9-4862-8a13-d3c75475bc6e",
+					SortOrder: 1,
+				},
+				{
+					PlaceID:   "339809cf-d515-4a64-bbcd-c6a899051273",
+					PlanID:    "4a81310e-03c9-4862-8a13-d3c75475bc6e",
+					SortOrder: 0,
+				},
+				{
+					// 異なるプラン
+					PlaceID:   "d0a3e6b7-11c6-4050-8d3f-68220349a8a7",
+					PlanID:    "d0a3e6b7-11c6-4050-8d3f-68220349a8a7",
+					SortOrder: 0,
+				},
+			},
+			places: []models.Place{
+				{Id: "339809cf-d515-4a64-bbcd-c6a899051273"},
+				{Id: "ec7c607d-454a-4644-929a-c3b1e078842d"},
+			},
+			expected: []models.Place{
+				{Id: "339809cf-d515-4a64-bbcd-c6a899051273"},
+				{Id: "ec7c607d-454a-4644-929a-c3b1e078842d"},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			actual, err := NewPlanPlacesFromEntities(c.planPlaceSlice, c.places, c.planId)
+			if err != nil {
+				t.Errorf("error converting entities to domain models: %v", err)
+			}
+
+			if diff := cmp.Diff(c.expected, *actual); diff != "" {
+				t.Errorf("wrong plan places (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestNewPlacesFromEntities_ShouldFail(t *testing.T) {
+	cases := []struct {
+		name           string
+		planPlaceSlice generated.PlanPlaceSlice
+		places         []models.Place
+		planId         string
+	}{
+		{
+			name:   "should fail if place not found",
+			planId: "4a81310e-03c9-4862-8a13-d3c75475bc6e",
+			planPlaceSlice: generated.PlanPlaceSlice{
+				{
+					PlaceID:   "ec7c607d-454a-4644-929a-c3b1e078842d",
+					PlanID:    "4a81310e-03c9-4862-8a13-d3c75475bc6e",
+					SortOrder: 1,
+				},
+				{
+					PlaceID:   "339809cf-d515-4a64-bbcd-c6a899051273",
+					PlanID:    "4a81310e-03c9-4862-8a13-d3c75475bc6e",
+					SortOrder: 0,
+				},
+			},
+			places: []models.Place{
+				{Id: "339809cf-d515-4a64-bbcd-c6a899051273"},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			_, err := NewPlanPlacesFromEntities(c.planPlaceSlice, c.places, c.planId)
+			if err == nil {
+				t.Errorf("error should be returned")
 			}
 		})
 	}
