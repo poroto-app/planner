37b2629770522a6f54d1cd6861985f8e27b3bd0c
zacker <robomasaki2@gmail.com>
2024-01-07 14:21:27 +0900

    feat(data): sqlboilerのログをzap経由で出力する

--- internal/infrastructure/rdb/init.go
+++ internal/infrastructure/rdb/init.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"go.uber.org/zap"
 	"os"
+	"poroto.app/poroto/planner/internal/domain/utils"
 )
 
 func InitDB(debugMode bool) (*sql.DB, error) {
@@ -27,7 +29,20 @@ func InitDB(debugMode bool) (*sql.DB, error) {
 	boil.SetDB(db)
 
 	if debugMode {
+		logger, err := utils.NewLogger(utils.LoggerOption{
+			Tag: "sqlboiler",
+		})
+		if err != nil {
+			return nil, fmt.Errorf("error while creating logger: %v\n", err)
+		}
+
+		stdOutLogger, err := zap.NewStdLogAt(logger, zap.DebugLevel)
+		if err != nil {
+			return nil, fmt.Errorf("error while creating logger: %v\n", err)
+		}
+
 		boil.DebugMode = true
+		boil.DebugWriter = stdOutLogger.Writer()
 	}
 
 	return db, nil
