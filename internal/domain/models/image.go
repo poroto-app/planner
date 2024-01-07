23b5fcaa53116c45d74c55ac01110853060a3819
zacker <robomasaki2@gmail.com>
2024-01-07 17:27:13 +0900

    refactor(domain): 使われていない関数を削除

--- internal/domain/models/image.go
+++ internal/domain/models/image.go
@@ -1,10 +1,5 @@
 package models
 
-import (
-	"fmt"
-	"poroto.app/poroto/planner/internal/domain/utils"
-)
-
 type Image struct {
 	Width  uint
 	Height uint
@@ -14,17 +9,6 @@ type Image struct {
 type ImageSmallLarge struct {
 	Small *string
 	Large *string
-}
-
-func NewImage(small, large *string) (*ImageSmallLarge, error) {
-	if small == nil && large == nil {
-		return nil, fmt.Errorf("small and large are both nil")
-	}
-
-	return &ImageSmallLarge{
-		Small: utils.StrCopyPointerValue(small),
-		Large: utils.StrCopyPointerValue(large),
-	}, nil
 }
 
 // Default は，画像のデフォルトのURLを返す
