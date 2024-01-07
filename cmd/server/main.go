ece4b861c264539bf40e6d46577f63d563563037
zacker <robomasaki2@gmail.com>
2024-01-07 10:19:45 +0900

    feat(graphql): graphqlのResolverにDBを持たせる

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"os"
 	"poroto.app/poroto/planner/internal/env"
+	"poroto.app/poroto/planner/internal/infrastructure/rdb"
 
 	"poroto.app/poroto/planner/internal/interface/rest"
 )
@@ -13,8 +15,13 @@ func init() {
 }
 
 func main() {
+	db, err := rdb.InitDB(os.Getenv("ENV") == "development")
+	if err != nil {
+		log.Fatalf("error while initializing db: %v", err)
+	}
+
 	s := rest.NewRestServer(os.Getenv("ENV"))
-	if err := s.ServeHTTP(); err != nil {
+	if err := s.ServeHTTP(db); err != nil {
 		log.Fatalf("error while starting server: %v", err)
 	}
 }
