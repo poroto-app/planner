e9b40c4261e537d5194a504f11617fa351fc91ba
zacker <robomasaki2@gmail.com>
2024-01-07 14:11:27 +0900

    test(domain): 値に重複があったときのテストを追加

--- internal/infrastructure/rdb/place_test.go
+++ internal/infrastructure/rdb/place_test.go
@@ -9,7 +9,6 @@ import (
 	"poroto.app/poroto/planner/internal/domain/models"
 	"poroto.app/poroto/planner/internal/domain/utils"
 	"poroto.app/poroto/planner/internal/infrastructure/rdb/generated"
-	"poroto.app/poroto/planner/internal/infrastructure/rdb/mock"
 	"testing"
 	"time"
 )
@@ -20,12 +19,136 @@ func TestPlaceRepository_SavePlacesFromGooglePlace(t *testing.T) {
 		googlePlace models.GooglePlace
 	}{
 		{
-			name:        "save places from google place with nearby search result",
-			googlePlace: mock.NewMockGooglePlaceTokyo(false, false),
+			name: "save places from google place with nearby search result",
+			googlePlace: models.GooglePlace{
+				PlaceId:  "ChIJ7WoyEQr9GGAREzlMT6J-JhA",
+				Name:     "東京駅",
+				Location: models.GeoLocation{Latitude: 35.6812362, Longitude: 139.7649361},
+				Types:    []string{"train_station", "transit_station", "point_of_interest", "establishment"},
+				PhotoReferences: []models.GooglePlacePhotoReference{
+					{
+						PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+						Width:            4032,
+						Height:           3024,
+						HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+					},
+				},
+				PriceLevel:       2,
+				Rating:           4.300000190734863,
+				UserRatingsTotal: 100,
+				Vicinity:         utils.StrPointer("日本、〒100-0005 東京都千代田区丸の内１丁目９−１"),
+				Photos:           nil,
+				PlaceDetail:      nil,
+			},
 		},
 		{
-			name:        "save places from google place with place detail result",
-			googlePlace: mock.NewMockGooglePlaceTokyo(true, true),
+			name: "save places from google place with place detail result",
+			googlePlace: models.GooglePlace{
+				PlaceId:  "ChIJ7WoyEQr9GGAREzlMT6J-JhA",
+				Name:     "東京駅",
+				Location: models.GeoLocation{Latitude: 35.6812362, Longitude: 139.7649361},
+				Types:    []string{"train_station", "transit_station", "point_of_interest", "establishment"},
+				PhotoReferences: []models.GooglePlacePhotoReference{
+					{
+						PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+						Width:            4032,
+						Height:           3024,
+						HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+					},
+				},
+				PriceLevel:       2,
+				Rating:           4.300000190734863,
+				UserRatingsTotal: 100,
+				Vicinity:         utils.StrPointer("日本、〒100-0005 東京都千代田区丸の内１丁目９−１"),
+				Photos: &[]models.GooglePlacePhoto{
+					{
+						PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+						Width:            4032,
+						Height:           3024,
+						HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+						Small:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-1=s1600-w4032-h3024"),
+						Large:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-1=s1600-w1000-h1000"),
+					},
+					{
+
+						PhotoReference:   "photo-2-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+						Width:            1920,
+						Height:           1080,
+						HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100755868001879781001\">A Google User</a>"},
+						Small:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-2=s1600-w1920-h1080"),
+						Large:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-2=s1600-w1000-h1000"),
+					},
+				},
+				PlaceDetail: &models.GooglePlaceDetail{
+					OpeningHours: &models.GooglePlaceOpeningHours{
+						Periods: []models.GooglePlaceOpeningPeriod{
+							{DayOfWeekOpen: "Monday", DayOfWeekClose: "Monday", OpeningTime: "1030", ClosingTime: "2130"},
+							{DayOfWeekOpen: "Tuesday", DayOfWeekClose: "Tuesday", OpeningTime: "1030", ClosingTime: "2130"},
+							{DayOfWeekOpen: "Wednesday", DayOfWeekClose: "Wednesday", OpeningTime: "1030", ClosingTime: "2130"},
+							{DayOfWeekOpen: "Thursday", DayOfWeekClose: "Thursday", OpeningTime: "1030", ClosingTime: "2130"},
+							{DayOfWeekOpen: "Friday", DayOfWeekClose: "Friday", OpeningTime: "1030", ClosingTime: "2130"},
+							{DayOfWeekOpen: "Saturday", DayOfWeekClose: "Saturday", OpeningTime: "1030", ClosingTime: "2130"},
+						},
+					},
+					PhotoReferences: []models.GooglePlacePhotoReference{
+						// Place Detailで取得される値は一部、Nearby Searchで取得される値と重複する
+						{
+							PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+							Width:            4032,
+							Height:           3024,
+							HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+						},
+						{
+							PhotoReference:   "photo-2-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+							Width:            1920,
+							Height:           1080,
+							HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100755868001879781001\">A Google User</a>"},
+						},
+					},
+					Reviews: []models.GooglePlaceReview{
+						{
+							Rating:                4,
+							Text:                  utils.StrPointer("とても大きな駅です。地下街も広く、お店もたくさんあります。駅員さんも多く、親切です。"),
+							Time:                  1648126226,
+							AuthorName:            "Alice Alicia",
+							AuthorProfileImageUrl: utils.StrPointer("https://lh3.googleusercontent.com/a/ACg8ocKaPr9FWIiqs88c_Fugafugafugafugagfuagaufaugafufa=s128-c0x00000000-cc-rp-mo-ba5"),
+							AuthorUrl:             utils.StrPointer("https://www.google.com/maps/contrib/117028493732372946396/reviews"),
+						},
+						{
+							Rating:                5,
+							Text:                  utils.StrPointer("近くに住んでいるので、よく利用しています。駅員さんも親切で、地下街も広く、お店もたくさんあります。"),
+							Time:                  1618085426,
+							AuthorName:            "Bob Bobson",
+							AuthorProfileImageUrl: utils.StrPointer("https://lh3.googleusercontent.com/a-/ALV-HOGEhogehogehoge_wD8wQ5y5NPqCU7qZM9rnp00GHZYagec=s128-c0x00000000-cc-rp-mo-ba4"),
+							AuthorUrl:             utils.StrPointer("https://www.google.com/maps/contrib/2849473937494373893093/reviews"),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "save places from google place with duplicated values",
+			googlePlace: models.GooglePlace{
+				PlaceId:  "ChIJ7WoyEQr9GGAREzlMT6J-JhA",
+				Name:     "東京駅",
+				Location: models.GeoLocation{Latitude: 35.6812362, Longitude: 139.7649361},
+				Types:    []string{"train_station", "transit_station", "point_of_interest", "establishment"},
+				PhotoReferences: []models.GooglePlacePhotoReference{
+					{
+						PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+						Width:            4032,
+						Height:           3024,
+						HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+					},
+					// 重複した値
+					{
+						PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+						Width:            4032,
+						Height:           3024,
+						HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+					},
+				},
+			},
 		},
 	}
 
@@ -915,6 +1038,52 @@ func TestPlaceRepository_SaveGooglePlacePhotos(t *testing.T) {
 						},
 					},
 					Photos: &[]models.GooglePlacePhoto{
+						{
+							PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+							Width:            4032,
+							Height:           3024,
+							HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+							Small:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-1=s1600-w4032-h3024"),
+							Large:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-1=s1600-w1000-h1000"),
+						},
+					},
+				},
+			},
+			googlePlaceId: "ChIJ7WoyEQr9GGAREzlMT6J-JhA",
+			googlePlacePhotos: []models.GooglePlacePhoto{
+				{
+					PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+					Width:            4032,
+					Height:           3024,
+					HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+					Small:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-1=s1600-w4032-h3024"),
+					Large:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-1=s1600-w1000-h1000"),
+				},
+			},
+		},
+		{
+			name: "save duplicated photos",
+			savedPlace: models.Place{
+				Id: uuid.New().String(),
+				Google: models.GooglePlace{
+					PlaceId: "ChIJ7WoyEQr9GGAREzlMT6J-JhA",
+					PhotoReferences: []models.GooglePlacePhotoReference{
+						{
+							PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+							Width:            4032,
+							Height:           3024,
+							HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+						},
+					},
+					Photos: &[]models.GooglePlacePhoto{
+						{
+							PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
+							Width:            4032,
+							Height:           3024,
+							HTMLAttributions: []string{"<a href=\"https://maps.google.com/maps/contrib/100969420913538879622\">A Google User</a>"},
+							Small:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-1=s1600-w4032-h3024"),
+							Large:            utils.StrPointer("https://lh3.googleusercontent.com/places/photo-1=s1600-w1000-h1000"),
+						},
 						{
 							PhotoReference:   "photo-1-AWU5eFjiROQJEeMpt7Hh2Pv-fdsabvls-wKBKNsJwobLXjjnbzXSBxTTW3bOtTbsrxkaoE1xx8RU3XFzv64gtTL137nfZtz0YAwpRsWThU7FtEpuJ3xGYOEQ2BFIHKLF5OLpVoGUybE-NryBdtAF7MDlYwBS7XACG",
 							Width:            4032,
