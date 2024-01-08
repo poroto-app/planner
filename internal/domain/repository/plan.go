563d26525937208d58bf784f15b9cd59e7952105
zacker <robomasaki2@gmail.com>
2024-01-08 14:57:49 +0900

    feat(data): 返り値としてクエリカーソルを渡す

--- internal/domain/repository/plan.go
+++ internal/domain/repository/plan.go
@@ -6,11 +6,17 @@ import (
 	"poroto.app/poroto/planner/internal/domain/models"
 )
 
+type SortedByCreatedAtQueryCursor string
+
 type PlanRepository interface {
 	Save(ctx context.Context, plan *models.Plan) error
-	SortedByCreatedAt(ctx context.Context, queryCursor *string, limit int) (*[]models.Plan, error)
+
+	SortedByCreatedAt(ctx context.Context, queryCursor *SortedByCreatedAtQueryCursor, limit int) (*[]models.Plan, *SortedByCreatedAtQueryCursor, error)
+
 	Find(ctx context.Context, planId string) (*models.Plan, error)
+
 	FindByAuthorId(ctx context.Context, authorId string) (*[]models.Plan, error)
+
 	// SortedByLocation location で指定した地点に近いプランを返す
 	SortedByLocation(ctx context.Context, location models.GeoLocation, queryCursor *string, limit int) (*[]models.Plan, *string, error)
 }
--- internal/domain/services/plan/fetch_all.go
+++ internal/domain/services/plan/fetch_all.go
@@ -3,14 +3,23 @@ package plan
 import (
 	"context"
 	"fmt"
+	"poroto.app/poroto/planner/internal/domain/repository"
 
 	"poroto.app/poroto/planner/internal/domain/models"
 )
 
-func (s Service) FetchPlans(ctx context.Context, nextPageToken *string) (*[]models.Plan, error) {
-	plans, err := s.planRepository.SortedByCreatedAt(ctx, nextPageToken, 10)
+func (s Service) FetchPlans(ctx context.Context, pageToken *string) (*[]models.Plan, error) {
+	var queryCursor *repository.SortedByCreatedAtQueryCursor
+	if pageToken != nil {
+		qc := repository.SortedByCreatedAtQueryCursor(*pageToken)
+		queryCursor = &qc
+	}
+
+	// TODO: PageTokenをreturnする
+	plans, _, err := s.planRepository.SortedByCreatedAt(ctx, queryCursor, 10)
 	if err != nil {
 		return nil, fmt.Errorf("error while fetching plans: %v", err)
 	}
+
 	return plans, nil
 }
