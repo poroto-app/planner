e1660e05ceb8ea03316fecaf1f4b94670bf23c54
zacker <robomasaki2@gmail.com>
2024-03-03 16:39:51 +0900

    refactor(domain): 変数名を修正

--- internal/domain/services/plangen/select_base_place.go
+++ internal/domain/services/plangen/select_base_place.go
@@ -25,7 +25,7 @@ type SelectBasePlaceInput struct {
 	Radius                 int
 }
 
-// SelectBasePlace は，プランの起点となる場所を選択する
+// SelectBasePlace は，プランの起点となる場所の候補を選択する
 func (s Service) SelectBasePlace(input SelectBasePlaceInput) []models.Place {
 	s.logger.Debug(
 		"SelectBasePlace",
@@ -46,48 +46,48 @@ func (s Service) SelectBasePlace(input SelectBasePlaceInput) []models.Place {
 		panic("base location is zero value")
 	}
 
-	places := input.Places
+	placesFiltered := input.Places
 
 	// レビューが低い、またはレビュー数が少ない場所を除外する
-	places = placefilter.FilterByRating(places, 3.0, 10)
-	s.logger.Debug("Places after filtering by rating", zap.Int("Places", len(places)))
+	placesFiltered = placefilter.FilterByRating(placesFiltered, 3.0, 10)
+	s.logger.Debug("Places after filtering by rating", zap.Int("Places", len(placesFiltered)))
 
 	// ユーザーが拒否した場所は取り除く
 	if input.CategoryNamesDisliked != nil {
 		categoriesDisliked := models.GetCategoriesFromSubCategories(*input.CategoryNamesDisliked)
-		places = placefilter.FilterByCategory(places, categoriesDisliked, false)
-		s.logger.Debug("Places after filtering by disliked categories", zap.Int("Places", len(places)))
+		placesFiltered = placefilter.FilterByCategory(placesFiltered, categoriesDisliked, false)
+		s.logger.Debug("Places after filtering by disliked categories", zap.Int("Places", len(placesFiltered)))
 	}
 
 	// すでに選択された場所は除外
-	places = array.Filter(places, func(place models.Place) bool {
+	placesFiltered = array.Filter(placesFiltered, func(place models.Place) bool {
 		_, isIgnorePlace := array.Find(input.IgnorePlaces, func(p models.Place) bool {
 			return p.Google.PlaceId == place.Google.PlaceId
 		})
 		return !isIgnorePlace
 	})
-	s.logger.Debug("Places after filtering ignore places", zap.Int("Places", len(places)))
+	s.logger.Debug("Places after filtering ignore places", zap.Int("places", len(placesFiltered)))
 
-	places = placefilter.FilterDefaultIgnore(placefilter.FilterDefaultIgnoreInput{
-		Places:              places,
+	placesFiltered = placefilter.FilterDefaultIgnore(placefilter.FilterDefaultIgnoreInput{
+		Places:              placesFiltered,
 		StartLocation:       input.BaseLocation,
 		IgnoreDistanceRange: float64(input.Radius),
 	})
-	s.logger.Debug("Places after filtering default ignore", zap.Int("Places", len(places)))
+	s.logger.Debug("Places after filtering default ignore", zap.Int("places", len(placesFiltered)))
 
 	placesSelected := make([]models.Place, 0, input.MaxBasePlaceCount)
-	for len(placesSelected) < input.MaxBasePlaceCount || len(places) > 0 {
+	for len(placesSelected) < input.MaxBasePlaceCount || len(placesFiltered) > 0 {
 		// プラン間で重複が発生しないように、すでに選択された場所から500m以内の場所は選択しない
-		places = array.Filter(places, func(place models.Place) bool {
+		placesFiltered = array.Filter(placesFiltered, func(place models.Place) bool {
 			_, isDistanceWithIn := array.Find(placesSelected, func(p models.Place) bool {
 				return p.Location.DistanceInMeter(place.Location) < 500
 			})
 			return !isDistanceWithIn
 		})
-		s.logger.Debug("Places after filtering by distance from selected places", zap.Int("Places", len(places)))
+		s.logger.Debug("Places after filtering by distance from selected places", zap.Int("Places", len(placesFiltered)))
 
 		// レビューの最も高い場所を選択する
-		placesSortedByRating := models.SortPlacesByRating(places)
+		placesSortedByRating := models.SortPlacesByRating(placesFiltered)
 		if len(placesSortedByRating) == 0 {
 			break
 		}
