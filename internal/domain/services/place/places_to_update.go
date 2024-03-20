5ce86946a0fcff416853176b78f4b496fbe8821f
0re2236 <2236takahashi@gmail.com>
2024-03-20 16:49:12 +0900

    トークンを用いてログイン状態のチェックを行う

--- internal/domain/services/place/places_to_update.go
+++ internal/domain/services/place/places_to_update.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"go.uber.org/zap"
 	"poroto.app/poroto/planner/internal/domain/models"
+	"poroto.app/poroto/planner/internal/domain/services/user"
 )
 
 type UploadPlacePhotoInPlanInput struct {
@@ -22,6 +24,20 @@ func (s Service) UploadPlacePhotoInPlan(
 ) error {
 	var placePhotos []models.PlacePhoto
 	for _, input := range inputs {
+		checkAuthStateResult, err := s.userService.CheckUserAuthState(ctx, user.CheckUserAuthStateInput{
+			UserId:            input.UserId,
+			FirebaseAuthToken: input.FirebaseAuthToken,
+		})
+		if err != nil {
+			s.logger.Error("error while checking user auth state", zap.Error(err))
+			continue
+		}
+
+		if !checkAuthStateResult.IsAuthenticated {
+			s.logger.Error("user is not authenticated", zap.String("userId", input.UserId), zap.String("firebaseAuthToken", input.FirebaseAuthToken))
+			continue
+		}
+
 		placePhotos = append(placePhotos, models.PlacePhoto{
 			PlaceId:  input.PlaceId,
 			UserId:   input.UserId,
