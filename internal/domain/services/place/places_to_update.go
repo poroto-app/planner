47faa3cc4b4c6a99de86aa8ff0f3c8f8f96aec8f
0re2236 <2236takahashi@gmail.com>
2024-04-05 14:17:32 +0900

    Refactor place photo upload in planner service

--- internal/domain/services/place/places_to_update.go
+++ internal/domain/services/place/places_to_update.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"go.uber.org/zap"
 	"poroto.app/poroto/planner/internal/domain/models"
 	"poroto.app/poroto/planner/internal/domain/services/user"
 )
@@ -23,21 +22,17 @@ func (s Service) UploadPlacePhotoInPlan(
 	inputs []UploadPlacePhotoInPlanInput,
 ) error {
 	var placePhotos []models.PlacePhoto
+	checkAuthStateResult, err := s.userService.CheckUserAuthState(ctx, user.CheckUserAuthStateInput{
+		UserId:            userId,
+		FirebaseAuthToken: firebaseAuthToken,
+	})
+	if err != nil {
+		return fmt.Errorf("error while checking user auth state: %v", err)
+	}
+	if !checkAuthStateResult.IsAuthenticated {
+		return fmt.Errorf("user is not authenticated: user id: %s", userId)
+	}
 	for _, input := range inputs {
-		checkAuthStateResult, err := s.userService.CheckUserAuthState(ctx, user.CheckUserAuthStateInput{
-			UserId:            userId,
-			FirebaseAuthToken: firebaseAuthToken,
-		})
-		if err != nil {
-			s.logger.Error("error while checking user auth state", zap.Error(err))
-			continue
-		}
-
-		if !checkAuthStateResult.IsAuthenticated {
-			s.logger.Error("user is not authenticated", zap.String("userId", userId), zap.String("firebaseAuthToken", firebaseAuthToken))
-			continue
-		}
-
 		placePhotos = append(placePhotos, models.PlacePhoto{
 			PlaceId:  input.PlaceId,
 			UserId:   userId,
@@ -46,7 +41,7 @@ func (s Service) UploadPlacePhotoInPlan(
 			Height:   input.Height,
 		})
 	}
-	err := s.placeRepository.SavePlacePhotos(ctx, placePhotos)
+	err = s.placeRepository.SavePlacePhotos(ctx, placePhotos)
 	if err != nil {
 		return fmt.Errorf("error while saving place photos: %v", err)
 	}
