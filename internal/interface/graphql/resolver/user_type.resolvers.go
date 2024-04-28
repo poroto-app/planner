851eeb0e3285c6a366887f129db11bc8f8c434ae
zacker <iszkmasaki@gmail.com>
2024-04-28 14:25:20 +0900

    feat(graphql): ユーザーがいいねした場所を取得できるようにする

--- internal/interface/graphql/resolver/user_type.resolvers.go
+++ internal/interface/graphql/resolver/user_type.resolvers.go
@@ -8,6 +8,11 @@ import (
 	"context"
 	"fmt"
 	"go.uber.org/zap"
+	"poroto.app/poroto/planner/internal/domain/array"
+	"poroto.app/poroto/planner/internal/domain/models"
+	"poroto.app/poroto/planner/internal/domain/services/user"
+	"poroto.app/poroto/planner/internal/domain/utils"
+	gcontext "poroto.app/poroto/planner/internal/interface/graphql/context"
 	"poroto.app/poroto/planner/internal/interface/graphql/factory"
 
 	"poroto.app/poroto/planner/internal/interface/graphql/generated"
@@ -16,6 +21,7 @@ import (
 
 // Plans is the resolver for the plans field.
 func (r *userResolver) Plans(ctx context.Context, obj *model.User) ([]*model.Plan, error) {
+	// TODO: N+1問題に対応する（https://blog.giftee.dev/2023-11-10-gqlgen-dataloader/）
 	r.Logger.Info("User#Plans", zap.String("userId", obj.ID))
 
 	author, err := r.UserService.FindByUserId(ctx, obj.ID)
@@ -39,7 +45,39 @@ func (r *userResolver) Plans(ctx context.Context, obj *model.User) ([]*model.Pla
 
 // LikedPlaces is the resolver for the likedPlaces field.
 func (r *userResolver) LikedPlaces(ctx context.Context, obj *model.User) ([]*model.Place, error) {
-	panic(fmt.Errorf("not implemented: LikedPlaces - likedPlaces"))
+	r.Logger.Info("User#LikedPlaces", zap.String("userId", obj.ID))
+
+	// ログインユーザーでない場合は空配列を返す
+	authUser := gcontext.GetAuthUser(ctx)
+	if authUser == nil {
+		r.Logger.Debug("auth user is nil")
+		return []*model.Place{}, nil
+	}
+
+	// ログインユーザーとリクエストユーザーが異なる場合は空配列を返す
+	if authUser.Id != obj.ID {
+		r.Logger.Debug(
+			"auth user and request user are different",
+			zap.String("authUserId", authUser.Id),
+			zap.String("requestUserId", obj.ID),
+		)
+		return []*model.Place{}, nil
+	}
+
+	places, err := r.UserService.FindLikePlaces(ctx, user.FindLikedPlacesInput{
+		UserId:    obj.ID,
+		CheckAuth: utils.ToPointer(false),
+	})
+	if err != nil {
+		r.Logger.Error("error while fetching liked places", zap.Error(err))
+		return nil, fmt.Errorf("internal server error")
+	}
+
+	graphqlPlaces := array.Map(*places, func(place models.Place) *model.Place {
+		return factory.PlaceFromDomainModel(&place)
+	})
+
+	return graphqlPlaces, nil
 }
 
 // User returns generated.UserResolver implementation.
