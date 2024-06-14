6ff5cf72181eeea15718a7765ba922dcdecb5814
zacker <iszkmasaki@gmail.com>
2024-06-14 19:00:43 +0900

    feat(db): MySQL 5.7に合わせてクエリを修正

--- db/migrations/20240608092636_add_parent_plan_id_to_plan_candidate.sql
+++ db/migrations/20240608092636_add_parent_plan_id_to_plan_candidate.sql
@@ -1,13 +1,21 @@
 -- +goose Up
--- +goose StatementBegin
+START TRANSACTION;
+
 ALTER TABLE plan_candidates
-    ADD COLUMN parent_plan_id CHAR(36) DEFAULT NULL,
+    ADD COLUMN parent_plan_id CHAR(36) DEFAULT NULL;
+
+ALTER TABLE plan_candidates
     ADD CONSTRAINT fk_plan_candidates_parent_plan_id FOREIGN KEY (parent_plan_id) REFERENCES plans(id);
--- +goose StatementEnd
+
+COMMIT;
 
 -- +goose Down
--- +goose StatementBegin
+START TRANSACTION;
+
 ALTER TABLE plan_candidates
-    DROP COLUMN parent_plan_id,
-    DROP CONSTRAINT fk_plan_candidates_parent_plan_id;
--- +goose StatementEnd
+    DROP FOREIGN KEY fk_plan_candidates_parent_plan_id;
+
+ALTER TABLE plan_candidates
+    DROP COLUMN parent_plan_id;
+
+COMMIT;
