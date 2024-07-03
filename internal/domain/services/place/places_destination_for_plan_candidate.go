64de0eebdc933b23990b02f98bda3af6c30bcbdb
zacker <iszkmasaki@gmail.com>
2024-07-03 18:34:19 +0900

    feat(domain): 目的地を提示するQueryを実装

---
+++ internal/domain/services/place/places_destination_for_plan_candidate.go
@@ -0,0 +1,68 @@
+package place
+
+import (
+	"context"
+	"poroto.app/poroto/planner/internal/domain/array"
+	"poroto.app/poroto/planner/internal/domain/models"
+	"time"
+)
+
+const (
+	desfaultFetchDestinationPlacesForPlanCandidateLimit = 10
+)
+
+type FetchDestinationPlacesForPlanCandidateInput struct {
+	PlanCandidateSetId string
+	PlanCandidateId    string
+	Limit              int
+}
+
+// FetchDestinationPlacesForPlanCandidate カテゴリからプランを作成したときに、その条件をもとに他の目的地を提示する
+func (s Service) FetchDestinationPlacesForPlanCandidate(ctx context.Context, input FetchDestinationPlacesForPlanCandidateInput) (*[]models.Place, error) {
+	if input.Limit == 0 {
+		input.Limit = desfaultFetchDestinationPlacesForPlanCandidateLimit
+	}
+
+	planCandidate, err := s.planCandidateRepository.Find(ctx, input.PlanCandidateSetId, time.Now())
+	if err != nil {
+		return nil, err
+	}
+
+	// カテゴリからプランを作成したときのみ取得できるようにする
+	if planCandidate.MetaData.CreateByCategoryMetaData == nil {
+		return &[]models.Place{}, nil
+	}
+
+	googleCategoryTypes := planCandidate.MetaData.CreateByCategoryMetaData.Category.GooglePlaceTypes
+	if len(googleCategoryTypes) == 0 {
+		return &[]models.Place{}, nil
+	}
+
+	placesInPlans := array.FlatMap(planCandidate.Plans, func(plan models.Plan) []models.Place {
+		return plan.Places
+	})
+
+	placesOfCategory, err := s.placeRepository.FindByGooglePlaceType(
+		ctx,
+		googleCategoryTypes[0],
+		planCandidate.MetaData.CreateByCategoryMetaData.Location,
+		planCandidate.MetaData.CreateByCategoryMetaData.RadiusInKm*1000,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// すでにプランに含まれている場所を提示しない
+	placesDestination := array.Filter(*placesOfCategory, func(place models.Place) bool {
+		_, found := array.Find(placesInPlans, func(p models.Place) bool {
+			return p.Id == place.Id
+		})
+		return !found
+	})
+
+	if len(placesDestination) > input.Limit {
+		placesDestination = placesDestination[:input.Limit]
+	}
+
+	return &placesDestination, nil
+}
