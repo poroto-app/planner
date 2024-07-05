94f29ef774e72187a72626a3f7dbe66cf15ec0bb
0re2236 <2236takahashi@gmail.com>
2024-07-05 16:10:18 +0900

    マイグレーションファイルの修正

--- db/migrations/20240703091106_create_place_photo_references_table.sql
+++ db/migrations/20240703091106_create_place_photo_references_table.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-START TRANSACTION;
+-- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS place_photo_references
 (
     id         CHAR(36) PRIMARY KEY,
@@ -10,26 +10,31 @@ CREATE TABLE IF NOT EXISTS place_photo_references
     FOREIGN KEY (place_id) REFERENCES places (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
+-- +goose StatementEnd
 
+-- +goose StatementBegin
 ALTER TABLE place_photos
     ADD COLUMN place_photo_reference_id CHAR(36) DEFAULT NULL;
+-- +goose StatementEnd
 
+-- +goose StatementBegin
 ALTER TABLE place_photos
-    ADD CONSTRAINT fk_place_photos_place_photo_reference_id FOREIGN KEY (place_photo_reference_id) REFERENCES place_photo_references (id);
-COMMIT;
-
+    ADD CONSTRAINT fk_place_photos_place_photo_reference_id FOREIGN KEY (place_photo_reference_id) REFERENCES place_photo_references(id);
+-- +goose StatementEnd
 
 
 
 -- +goose Down
-START TRANSACTION;
+-- +goose StatementBegin
 ALTER TABLE place_photos
     DROP FOREIGN KEY fk_place_photos_place_photo_reference_id;
+-- +goose StatementEnd
 
+-- +goose StatementBegin
 ALTER TABLE place_photos
     DROP COLUMN place_photo_reference_id;
+-- +goose StatementEnd
 
+-- +goose StatementBegin
 DROP TABLE IF EXISTS place_photo_references;
-COMMIT;
-
-
+-- +goose StatementEnd
